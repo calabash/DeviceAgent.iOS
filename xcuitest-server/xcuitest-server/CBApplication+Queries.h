4f3c7d53a1708a141af150fd41e8f16eefc54590
Christopher Fuentes <chris.fuentes@xamarin.com>
2016-02-03 13:14:50 -0800

    CBApplication+Queries

---
+++ xcuitest-server/xcuitest-server/CBApplication+Queries.h
@@ -0,0 +1,13 @@
+//
+//  CBApplication+Queries.h
+//  xcuitest-server
+//
+//  Created by Chris Fuentes on 2/3/16.
+//  Copyright © 2016 calabash. All rights reserved.
+//
+
+#import "CBApplication.h"
+
+@interface CBApplication (Queries)
+
+@end
---
+++ xcuitest-server/xcuitest-server/CBApplication+Queries.m
@@ -0,0 +1,13 @@
+//
+//  CBApplication+Queries.m
+//  xcuitest-server
+//
+//  Created by Chris Fuentes on 2/3/16.
+//  Copyright © 2016 calabash. All rights reserved.
+//
+
+#import "CBApplication+Queries.h"
+
+@implementation CBApplication (Queries)
+
+@end
