7cbae72b983940f24198c9ee042e62393e5d7090
Christopher Fuentes <chris.fuentes@xamarin.com>
2016-02-04 15:15:49 -0800

    Got project building again, re-added /coordinates endpoints

--- xcuitest-server/xcuitest-server/CBApplication+Gestures.h
+++ xcuitest-server/xcuitest-server/CBApplication+Gestures.h
@@ -8,4 +8,7 @@
 
 @interface CBApplication (Gestures)
 + (void)tap:(float)x :(float)y;
++ (void)doubleTap:(float)x :(float)y;
++ (void)press:(float)x :(float)y forDuration:(NSTimeInterval)duration;
++ (void)press:(float)x1 :(float)y1 forDuration:(NSTimeInterval)duration thenDragTo:(float)x2 :(float)y2;
 @end
--- xcuitest-server/xcuitest-server/GestureRoutes.m
+++ xcuitest-server/xcuitest-server/GestureRoutes.m
@@ -11,13 +11,55 @@
 @implementation GestureRoutes
 + (NSArray<CBRoute *> *)getRoutes {
     return @[
+             /*
+              *     Coordinates API
+              */
              [CBRoute post:@"/tap/coordinates" withBlock:^(RouteRequest *request, RouteResponse *response) {
                  NSDictionary *arguments = DATA_TO_JSON(request.body);
                  float x = [arguments[CB_X_KEY] floatValue],
                     y = [arguments[CB_Y_KEY] floatValue];
+                 
                  [CBApplication tap:x :y];
                  [response respondWithString:@"OK"];
-             }]
+             }],
+             
+             [CBRoute post:@"/doubleTap/coordinates" withBlock:^(RouteRequest *request, RouteResponse *response) {
+                 NSDictionary *arguments = DATA_TO_JSON(request.body);
+                 float x = [arguments[CB_X_KEY] floatValue],
+                    y = [arguments[CB_Y_KEY] floatValue];
+                 
+                 [CBApplication doubleTap:x :y];
+                 [response respondWithString:@"OK"];
+             }],
+             
+             [CBRoute post:@"/pressForDuration/coordinates" withBlock:^(RouteRequest *request, RouteResponse *response) {
+                 NSDictionary *arguments = DATA_TO_JSON(request.body);
+                 float x = [arguments[CB_X_KEY] floatValue],
+                    y = [arguments[CB_Y_KEY] floatValue],
+                 duration = [arguments[CB_DURATION_KEY] floatValue];
+                 
+                 [CBApplication press:x :y forDuration:duration];
+                 [response respondWithString:@"OK"];
+             }],
+             
+             [CBRoute post:@"/pressForDuration/coordinates/thenDragTo" withBlock:^(RouteRequest *request, RouteResponse *response) {
+                 NSDictionary *arguments = DATA_TO_JSON(request.body);
+                 float x1 = [arguments[CB_X1_KEY] floatValue],
+                    y1 = [arguments[CB_Y1_KEY] floatValue],
+                    duration = [arguments[CB_DURATION_KEY] floatValue],
+                    x2 = [arguments[CB_X2_KEY] floatValue],
+                    y2 = [arguments[CB_Y2_KEY] floatValue];
+                 
+                 [CBApplication press:x1 :y1 forDuration:duration thenDragTo:x2 :y2];
+                 [response respondWithString:@"OK"];
+             }],
+             
+             /*
+              *     Marked API
+              */
+             
+             //TODO
+             
              ];
 }
 
