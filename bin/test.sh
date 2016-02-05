8a1e7152b40f8648acdb35084eed44574a7d614d
Joshua Moody <joshuajmoody@gmail.com>
2016-02-05 11:15:45 +0100

    Test: compilation should fail on warnings

--- bin/test.sh
+++ bin/test.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
-xcodebuild -project ./xcuitest-server/xcuitest-server.xcodeproj \
- -target xcuitest-serverUITests -sdk iphonesimulator9.2 -arch x86_64
+xcodebuild \
+  -project ./xcuitest-server/xcuitest-server.xcodeproj \
+ -target xcuitest-serverUITests \
+ -sdk iphonesimulator9.2 \
+  GCC_TREAT_WARNINGS_AS_ERRORS=YES \
+ -arch x86_64
+
