c2705a3244c3065ae5a281c587965ab86830ac44
Joshua Moody <joshuajmoody@gmail.com>
2016-02-05 10:55:35 +0100

    GCDAsyncSock: fix undefined selector

--- CocoaHTTPServer/Vendor/CocoaAsyncSocket/GCDAsyncSocket.m
+++ CocoaHTTPServer/Vendor/CocoaAsyncSocket/GCDAsyncSocket.m
@@ -6754,12 +6754,13 @@ static OSStatus SSLWriteFunction(SSLConnectionRef connection, const void *data, 
 	[[NSThread currentThread] setName:GCDAsyncSocketThreadName];
 	
 	LogInfo(@"CFStreamThread: Started");
-	
+
+  SEL selector = NSSelectorFromString(@"doNothingAtAll:");
 	// We can't run the run loop unless it has an associated input source or a timer.
 	// So we'll just create a timer that will never fire - unless the server runs for decades.
 	[NSTimer scheduledTimerWithTimeInterval:[[NSDate distantFuture] timeIntervalSinceNow]
 	                                 target:self
-	                               selector:@selector(doNothingAtAll:)
+	                               selector:selector
 	                               userInfo:nil
 	                                repeats:YES];
 	
