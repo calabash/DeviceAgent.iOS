7b3b3cdb5ab8bd66556047f4bb1cbfdd4f8aacd5
Christopher Fuentes <chris.fuentes@xamarin.com>
2016-02-11 15:23:16 +0100

    refac tests.sh

--- bin/test.sh
+++ bin/test.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+bin/deps.sh
+
 xcodebuild \
   -project ./xcuitest-server/xcuitest-server.xcodeproj \
  -target xcuitest-serverUITests \
@@ -7,7 +9,7 @@ xcodebuild \
   GCC_TREAT_WARNINGS_AS_ERRORS=YES \
  -arch x86_64
 
-SIM_ID=$(instruments -w devices 2>&1 | grep "iPhone 6 (9.2)" |\
+SIM_ID=$(instruments -w devices 2>&1 | grep "iPhone 6 (9.0)" |\
            cut -d[ -f2 | cut -d] -f1)
 
 echo "Testing on $SIM_ID"
