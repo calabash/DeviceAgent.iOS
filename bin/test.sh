17f5f9da0cfe7dabea16f5d554cbac6bc1f6da7c
Chris Fuentes <cfuentes@g.harvard.edu>
2016-02-14 22:32:15 +0100

    Updated test.sh with new project/scheme name

--- bin/test.sh
+++ bin/test.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 xcodebuild \
-  -project ./xcuitest-server/xcuitest-server.xcodeproj \
- -target xcuitest-serverUITests \
+  -project CBXDriver.xcodeproj \
+ -target XCUITestDriver \
  -sdk iphonesimulator9.2 \
   GCC_TREAT_WARNINGS_AS_ERRORS=YES \
  -arch x86_64
