a94e5498e670eeab1a85e216f0904bf45ecd99fc
Christopher Fuentes <chris.fuentes@xamarin.com>
2016-03-30 13:51:12 -0700

    Force application resolution

--- Server/Application/CBApplication.m
+++ Server/Application/CBApplication.m
@@ -37,7 +37,10 @@ static NSInteger currentElementIndex = 0;
      *
      * TODO: ensure that the app is always resolved before a query
      */
-    [currentApplication.app resolve];
+    if ([currentApplication.app lastSnapshot] == nil) {
+        [[currentApplication.app applicationQuery] elementBoundByIndex:0];
+        [currentApplication.app resolve];
+    }
     
     return currentApplication.app;
 }
