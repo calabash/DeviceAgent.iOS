d722ed84b402db10f0f77a50dbe93dd096b9ac33
Joshua Moody <joshuajmoody@gmail.com>
2016-04-14 08:38:52 +0200

    UnitTest: rename gesture recognizer variables

--- UnitTest/UnitTestApp/FirstViewController.m
+++ UnitTest/UnitTestApp/FirstViewController.m
@@ -2,13 +2,13 @@
 #import "FirstViewController.h"
 
 @interface FirstViewController ()<UIGestureRecognizerDelegate>
-@property (nonatomic, strong) UIPanGestureRecognizer *panGR;
-@property (nonatomic, strong) UISwipeGestureRecognizer *swipeGR;
-@property (nonatomic, strong) UITapGestureRecognizer *tapGR;
-@property (nonatomic, strong) UITapGestureRecognizer *doubleTapGR;
-@property (nonatomic, strong) UIPinchGestureRecognizer *pinchGR;
-@property (nonatomic, strong) UIRotationGestureRecognizer *rotationGR;
-@property (nonatomic, strong) UILongPressGestureRecognizer *longPressGR;
+@property (nonatomic, strong) UIPanGestureRecognizer *panRecognizer;
+@property (nonatomic, strong) UISwipeGestureRecognizer *swipeRecognizer;
+@property (nonatomic, strong) UITapGestureRecognizer *tapRecognizer;
+@property (nonatomic, strong) UITapGestureRecognizer *doubleTapRecognizer;
+@property (nonatomic, strong) UIPinchGestureRecognizer *pinchRecognizer;
+@property (nonatomic, strong) UIRotationGestureRecognizer *rotationRecognizer;
+@property (nonatomic, strong) UILongPressGestureRecognizer *longPressRecognizer;
 
 @property (nonatomic, strong) NSMutableArray<CALayer *> *drawings;
 
@@ -31,44 +31,45 @@ static NSDictionary *colors;
 #define MatchGestureRecognizer(gr1, gr2, label) if (gr1 == gr2) { _gestureLabel.text = label; }
 
 - (void)viewDidLoad {
-  [super viewDidLoad];
+    [super viewDidLoad];
     
     _drawings = [NSMutableArray array];
     
-    _panGR =  MakeGestureRecognizer(UIPanGestureRecognizer);
-    _swipeGR = MakeGestureRecognizer(UISwipeGestureRecognizer);
-    _tapGR = MakeGestureRecognizer(UITapGestureRecognizer);
-    _pinchGR = MakeGestureRecognizer(UIPinchGestureRecognizer);
-    _rotationGR = MakeGestureRecognizer(UIRotationGestureRecognizer);
-    _longPressGR = MakeGestureRecognizer(UILongPressGestureRecognizer);
-    _doubleTapGR = MakeGestureRecognizer(UITapGestureRecognizer);
-    _doubleTapGR.numberOfTapsRequired = 2;
+    _panRecognizer =  MakeGestureRecognizer(UIPanGestureRecognizer);
+    _swipeRecognizer = MakeGestureRecognizer(UISwipeGestureRecognizer);
+    _tapRecognizer = MakeGestureRecognizer(UITapGestureRecognizer);
+    _pinchRecognizer = MakeGestureRecognizer(UIPinchGestureRecognizer);
+    _rotationRecognizer = MakeGestureRecognizer(UIRotationGestureRecognizer);
+    _longPressRecognizer = MakeGestureRecognizer(UILongPressGestureRecognizer);
+    _doubleTapRecognizer = MakeGestureRecognizer(UITapGestureRecognizer);
+    _doubleTapRecognizer.numberOfTapsRequired = 2;
     
-    for (UIGestureRecognizer *gr in @[_panGR,
-                                      _swipeGR,
-                                      _tapGR,
-                                      _pinchGR,
-                                      _rotationGR,
-                                      _longPressGR,
-                                      _doubleTapGR]) {
+    for (UIGestureRecognizer *gr in @[_panRecognizer,
+                                      _swipeRecognizer,
+                                      _tapRecognizer,
+                                      _pinchRecognizer,
+                                      _rotationRecognizer,
+                                      _longPressRecognizer,
+                                      _doubleTapRecognizer]) {
         [self.view addGestureRecognizer:gr];
     }
 }
 
-- (void)recognizeGesture:(UIGestureRecognizer *)gr {
-    if (gr.state == UIGestureRecognizerStateBegan || gr == _tapGR) {
+- (void)recognizeGesture:(UIGestureRecognizer *)recognizer {
+    if (recognizer.state == UIGestureRecognizerStateBegan ||
+        recognizer == _tapRecognizer) {
         for (CALayer *layer in _drawings) {
             [layer removeFromSuperlayer];
         }
     }
     
-    MatchGestureRecognizer(gr, _panGR, @"Pan");
-    MatchGestureRecognizer(gr, _swipeGR, @"Swipe");
-    MatchGestureRecognizer(gr, _tapGR, @"Tap");
-    MatchGestureRecognizer(gr, _pinchGR, @"Pinch");
-    MatchGestureRecognizer(gr, _rotationGR, @"Rotation");
-    MatchGestureRecognizer(gr, _longPressGR, @"Long Press");
-    MatchGestureRecognizer(gr, _doubleTapGR, @"Double Tap");
+    MatchGestureRecognizer(recognizer, _panRecognizer, @"Pan");
+    MatchGestureRecognizer(recognizer, _swipeRecognizer, @"Swipe");
+    MatchGestureRecognizer(recognizer, _tapRecognizer, @"Tap");
+    MatchGestureRecognizer(recognizer, _pinchRecognizer, @"Pinch");
+    MatchGestureRecognizer(recognizer, _rotationRecognizer, @"Rotation");
+    MatchGestureRecognizer(recognizer, _longPressRecognizer, @"Long Press");
+    MatchGestureRecognizer(recognizer, _doubleTapRecognizer, @"Double Tap");
     
     /*
      http://stackoverflow.com/questions/16846413/how-do-you-draw-a-line-programmatically-from-a-view-controller
@@ -77,9 +78,8 @@ static NSDictionary *colors;
         This becomes buggy if a finger leaves mid-gesture, but probably
         beyond the scope of our testing here. 
      */
-    
-    for (int i = 0; i < [gr numberOfTouches]; i++) {
-        CGPoint p = [gr locationOfTouch:i inView:self.view];
+    for (int i = 0; i < [recognizer numberOfTouches]; i++) {
+        CGPoint p = [recognizer locationOfTouch:i inView:self.view];
         UIBezierPath *path = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(p.x, p.y, 3, 3) cornerRadius:1];
         CAShapeLayer *shapeLayer = [CAShapeLayer layer];
         shapeLayer.path = [path CGPath];
@@ -90,7 +90,7 @@ static NSDictionary *colors;
         [_drawings addObject:shapeLayer];
         [self.view.layer addSublayer:shapeLayer];
     }
-    
+
     [self.view bringSubviewToFront:_gestureLabel];
 }
 
