bc003a328ed5704d30eac568f69f14388162b67e
Joshua Moody <joshuajmoody@gmail.com>
2016-04-14 09:45:33 +0200

    UnitTest: replace macros with method calls

--- UnitTest/UnitTestApp/FirstViewController.m
+++ UnitTest/UnitTestApp/FirstViewController.m
@@ -12,11 +12,14 @@
 
 @property (nonatomic, strong) NSMutableArray<CALayer *> *drawings;
 
+- (void)recognizeGesture:(UIGestureRecognizer *)recognizer;
+
 @end
 
 @implementation FirstViewController
 
 static NSDictionary *colors;
+
 + (void)initialize {
     colors = @{
                @(0) : [UIColor blueColor],
@@ -27,32 +30,65 @@ static NSDictionary *colors;
                };
 }
 
-#define MakeGestureRecognizer(class) [[class alloc] initWithTarget:self action:@selector(recognizeGesture:)]
-#define MatchGestureRecognizer(gr1, gr2, label) if (gr1 == gr2) { _gestureLabel.text = label; }
+- (void)updateGestureLabelText:(NSString *)text
+                    recognizer:(UIGestureRecognizer *)handledRecognizer {
+    for (UIGestureRecognizer *recognizer in self.view.gestureRecognizers) {
+        if (recognizer == handledRecognizer) {
+            self.gestureLabel.text = text;
+            break;
+        }
+    }
+}
+
+- (NSString *)textForRecognizer:(UIGestureRecognizer *)recognizer {
+    if (recognizer == self.panRecognizer) {
+        return @"Pan";
+    } else if (recognizer == self.tapRecognizer) {
+        return @"Swipe";
+    } else if (recognizer == self.pinchRecognizer) {
+        return @"Pinch";
+    } else if (recognizer == self.rotationRecognizer) {
+        return @"Rotation";
+    } else if (recognizer == self.longPressRecognizer) {
+        return @"Long Press";
+    } else if (recognizer == self.doubleTapRecognizer) {
+        return @"Double Tap";
+    } else {
+        return @"Unknown Gesture";
+    }
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
     _drawings = [NSMutableArray array];
-    
-    _panRecognizer =  MakeGestureRecognizer(UIPanGestureRecognizer);
-    _swipeRecognizer = MakeGestureRecognizer(UISwipeGestureRecognizer);
-    _tapRecognizer = MakeGestureRecognizer(UITapGestureRecognizer);
-    _pinchRecognizer = MakeGestureRecognizer(UIPinchGestureRecognizer);
-    _rotationRecognizer = MakeGestureRecognizer(UIRotationGestureRecognizer);
-    _longPressRecognizer = MakeGestureRecognizer(UILongPressGestureRecognizer);
-    _doubleTapRecognizer = MakeGestureRecognizer(UITapGestureRecognizer);
-    _doubleTapRecognizer.numberOfTapsRequired = 2;
-    
-    for (UIGestureRecognizer *gr in @[_panRecognizer,
-                                      _swipeRecognizer,
-                                      _tapRecognizer,
-                                      _pinchRecognizer,
-                                      _rotationRecognizer,
-                                      _longPressRecognizer,
-                                      _doubleTapRecognizer]) {
-        [self.view addGestureRecognizer:gr];
-    }
+
+    SEL selector = @selector(recognizeGesture:);
+    self.panRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self
+                                                                 action:selector];
+    [self.view addGestureRecognizer:self.panRecognizer];
+
+    self.swipeRecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self
+                                                                     action:selector];
+
+    [self.view addGestureRecognizer:self.swipeRecognizer];
+
+    self.tapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self
+                                                                 action:selector];
+    [self.view addGestureRecognizer:self.tapRecognizer];
+
+    self.pinchRecognizer = [[UIPinchGestureRecognizer alloc] initWithTarget:self
+                                                                     action:selector];
+    [self.view addGestureRecognizer:self.pinchRecognizer];
+
+    self.longPressRecognizer = [[UILongPressGestureRecognizer alloc] initWithTarget:self
+                                                                             action:selector];
+    [self.view addGestureRecognizer:self.longPressRecognizer];
+
+    self.doubleTapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self
+                                                                       action:selector];
+    self.doubleTapRecognizer.numberOfTapsRequired = 2;
+    [self.view addGestureRecognizer:self.doubleTapRecognizer];
 }
 
 - (void)recognizeGesture:(UIGestureRecognizer *)recognizer {
@@ -62,21 +98,16 @@ static NSDictionary *colors;
             [layer removeFromSuperlayer];
         }
     }
-    
-    MatchGestureRecognizer(recognizer, _panRecognizer, @"Pan");
-    MatchGestureRecognizer(recognizer, _swipeRecognizer, @"Swipe");
-    MatchGestureRecognizer(recognizer, _tapRecognizer, @"Tap");
-    MatchGestureRecognizer(recognizer, _pinchRecognizer, @"Pinch");
-    MatchGestureRecognizer(recognizer, _rotationRecognizer, @"Rotation");
-    MatchGestureRecognizer(recognizer, _longPressRecognizer, @"Long Press");
-    MatchGestureRecognizer(recognizer, _doubleTapRecognizer, @"Double Tap");
-    
+
+    NSString *text = [self textForRecognizer:recognizer];
+    [self updateGestureLabelText:text recognizer:recognizer];
+
     /*
      http://stackoverflow.com/questions/16846413/how-do-you-draw-a-line-programmatically-from-a-view-controller
      
-        Iterate through all touches, mapping each one to a different color. 
-        This becomes buggy if a finger leaves mid-gesture, but probably
-        beyond the scope of our testing here. 
+     Iterate through all touches, mapping each one to a different color.
+     This becomes buggy if a finger leaves mid-gesture, but probably
+     beyond the scope of our testing here.
      */
     for (int i = 0; i < [recognizer numberOfTouches]; i++) {
         CGPoint p = [recognizer locationOfTouch:i inView:self.view];
