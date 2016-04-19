a265e9c09587a269a517446c4e68d4bc6d9c09c3
Joshua Moody <joshuajmoody@gmail.com>
2016-04-19 20:29:24 +0200

    TwoFingerTap: fix typo in header docs

--- Server/TwoFingerTap.h
+++ Server/TwoFingerTap.h
@@ -2,7 +2,7 @@
 #import "Gesture+Options.h"
 
 /*  
-    Tap a cooridate with two fingers.   Use `duration` to set the hold time.
+    Tap a coordinate with two fingers.   Use `duration` to set the hold time.
  */
 @interface TwoFingerTap : Gesture<Gesture>
 @end
