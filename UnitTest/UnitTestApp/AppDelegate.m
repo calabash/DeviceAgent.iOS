62bee86ac8a24f4bdf11a25974001b778ebf5743
Joshua Moody <joshuajmoody@gmail.com>
2016-05-19 16:16:51 +0200

    UnitTestApp: show touches in view
    with Fingertip library

--- UnitTest/UnitTestApp/AppDelegate.m
+++ UnitTest/UnitTestApp/AppDelegate.m
@@ -1,10 +1,21 @@
 #import "AppDelegate.h"
+#import "MBFingerTipWindow.h"
 
 @interface AppDelegate ()
 
 @end
 
 @implementation AppDelegate
+
+- (UIWindow *)window {
+    if (!_window) {
+        MBFingerTipWindow *ftWindow = [[MBFingerTipWindow alloc]
+                                       initWithFrame:[[UIScreen mainScreen] bounds]];
+        ftWindow.alwaysShowTouches = YES;
+        _window = ftWindow;
+    }
+    return _window;
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   return YES;
