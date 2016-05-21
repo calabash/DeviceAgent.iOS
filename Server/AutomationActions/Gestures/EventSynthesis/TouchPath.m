5129773925997f4f3bee00954390ae91174dc76f
Joshua Moody <joshuajmoody@gmail.com>
2016-05-21 20:23:04 +0200

    Server: unit tests for TouchPath event synthesis

--- Server/AutomationActions/Gestures/EventSynthesis/TouchPath.m
+++ Server/AutomationActions/Gestures/EventSynthesis/TouchPath.m
@@ -5,6 +5,11 @@
 @property (nonatomic, strong) XCTouchPath *xcTouchPath;
 @property (nonatomic, strong) XCPointerEventPath *eventPath;
 @property (nonatomic) CGPoint lastPoint;
+
++ (XCTouchPath *)touchPathForFirstTouchPoint:(CGPoint)point
+                                 orientation:(NSInteger)orientation;
++ (XCPointerEventPath *)eventPathForFirstTouchPoint:(CGPoint)point;
+
 @end
 
 @implementation TouchPath
@@ -16,30 +21,46 @@
     return self;
 }
 
-
-+ (instancetype)withFirstTouchPoint:(CGPoint)firstTouchPoint orientation:(NSInteger)orientation {
++ (instancetype)withFirstTouchPoint:(CGPoint)firstTouchPoint
+                        orientation:(NSInteger)orientation {
     TouchPath *tp = [[TouchPath alloc] initWithOrientation:orientation];
     
     tp.lastPoint = firstTouchPoint;
     
-    tp.xcTouchPath = [[XCTouchPath alloc] initWithTouchDown:firstTouchPoint
-                                                orientation:orientation
-                                                     offset:0];
-    
-    tp.eventPath = [[XCPointerEventPath alloc] initForTouchAtPoint:firstTouchPoint
-                                                            offset:0];
+    tp.xcTouchPath = [TouchPath touchPathForFirstTouchPoint:firstTouchPoint
+                                                orientation:orientation];
+
+    tp.eventPath = [TouchPath eventPathForFirstTouchPoint:firstTouchPoint];
     return tp;
 }
 
 - (void)moveToNextPoint:(CGPoint)nextPoint afterSeconds:(CGFloat)seconds {
+    NSAssert((self.xcTouchPath && self.eventPath),
+             @"Expected xcTouchPath and eventPath to be non-nil");
     [self.xcTouchPath moveToPoint:nextPoint atOffset:seconds];
     [self.eventPath moveToPoint:nextPoint atOffset:seconds];
     self.lastPoint = nextPoint;
 }
 
 - (void)liftUpAfterSeconds:(CGFloat)seconds {
-    [self.xcTouchPath liftUpAtPoint:_lastPoint offset:seconds];
+    NSAssert((self.xcTouchPath && self.eventPath),
+             @"Expected xcTouchPath and eventPath to be non-nil");
+    // No good way to assert that lastPoint has been set; it will default to
+    // CGPointZero if not assigned.
+    [self.xcTouchPath liftUpAtPoint:self.lastPoint offset:seconds];
     [self.eventPath liftUpAtOffset:seconds];
+}
+
++ (XCTouchPath *)touchPathForFirstTouchPoint:(CGPoint)point
+                                 orientation:(NSInteger)orientation {
+    return [[XCTouchPath alloc] initWithTouchDown:point
+                                      orientation:orientation
+                                           offset:0.0];
+}
+
++ (XCPointerEventPath *)eventPathForFirstTouchPoint:(CGPoint)point {
+    return [[XCPointerEventPath alloc] initForTouchAtPoint:point
+                                                    offset:0.0];
 }
 
 @end
