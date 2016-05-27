0dfffd21ecb4aab38ab239b383fa632398459134
Joshua Moody <joshuajmoody@gmail.com>
2016-05-27 18:19:53 +0200

    Client: better handling of orientation edge cases

--- cucumber/features/steps/orientation.rb
+++ cucumber/features/steps/orientation.rb
@@ -2,16 +2,24 @@
 module UnitTestApp
   module Orientation
 
+    def upside_down_supported?
+      hash = @device_agent.runtime
+      if hash["iphone6"] || hash["iphone6+"]
+        false
+      else
+        true
+      end
+    end
+
     def rotate_and_expect(position)
       symbol = position.to_sym
       json = @device_agent.rotate_home_button_to(symbol)
       orientation = json["orientation"]
 
-      if [:top, :up].include?(symbol)
-        # We need runtime info about the device under test.
-        # Specifically, is it an iPhone 6* device; they don't
-        # support upside down orientations.
-        RunLoop.log_warn("Skipping test because upside down orientation is not supported on all devices")
+      if [:top, :up].include?(symbol) && !upside_down_supported?
+        message = "Upside down orientation is not supported iPhone 6*"
+        colored = RunLoop::Color.blue(message)
+        $stdout.puts("    #{colored}")
       else
         expected = expected_position(position)
         expect(orientation).to be == expected
