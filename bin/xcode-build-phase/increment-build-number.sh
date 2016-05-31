66ee6ed0b334d8a6d7310ae38a62f3c603244298
Joshua Moody <joshuajmoody@gmail.com>
2016-05-31 14:59:16 +0200

    CBXRunner: CFBundleShortVersionString pinned to CBXAppStub version

--- bin/xcode-build-phase/increment-build-number.sh
+++ bin/xcode-build-phase/increment-build-number.sh
@@ -6,6 +6,8 @@ HEADER="${PROJECT_DIR}/${INFOPLIST_PREFIX_HEADER}"
 PLIST="${PROJECT_DIR}/${INFOPLIST_FILE}"
 touch "${PLIST}"
 
+VERSION=`/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" $PLIST | tr -d '\n'`
+
 NEW_BUILD_NUMBER=`date +%s`
 
 cat > "${HEADER}" <<EOF
@@ -17,9 +19,17 @@ build of ${PRODUCT_NAME} using:
 
 $ date +%s
 
+The PRODUCT_VERSION is used to set the
+
+CBX-Runner.app/PlugIns/CBX.xctest/Info.plist
+
+version from the CBXAppStub version. It is ignored
+in other targets.
+
 This file should not be added to version control.
 */
 
 #define PRODUCT_BUILD_NUMBER ${NEW_BUILD_NUMBER}
+#define PRODUCT_VERSION ${VERSION}
 EOF
 
