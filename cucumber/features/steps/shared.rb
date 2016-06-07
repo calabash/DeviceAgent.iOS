89ba93430a9a16cdb650197439e4dd001c095ec6
Joshua Moody <joshuajmoody@gmail.com>
2016-06-07 08:39:32 +0200

    Cucumber: add extra sleep _once_ per launch
    See https://github.com/calabash/DeviceAgent.iOS/issues/100

--- cucumber/features/steps/shared.rb
+++ cucumber/features/steps/shared.rb
@@ -1,14 +1,33 @@
+module DeviceAgent
+  module Shared
+
+    @@app_ready = nil
+
+    def wait_for_app
+      return true if DeviceAgent::Shared.class_variable_get(:@@app_ready)
+
+      ["Touch", "Pan", "Rotate/Pinch", "Misc", "Tao"].each do |mark|
+        @waiter.wait_for_view(mark)
+      end
+
+      if RunLoop::Environment.ci?
+        delay = 15
+      else
+        delay = 5
+      end
+
+      RunLoop.log_debug("Adding #{delay} second sleep; app is not ready to receive touches?")
+      sleep(delay)
+
+      DeviceAgent::Shared.class_variable_set(:@@app_ready, true)
+    end
+  end
+end
+
+World(DeviceAgent::Shared)
 
 Given(/^the app has launched$/) do
-  ["Touch", "Pan", "Rotate/Pinch", "Misc", "Tao"].each do |mark|
-    @waiter.wait_for_view(mark)
-  end
-
-  if RunLoop::Environment.ci?
-    sleep(15)
-  else
-    sleep(5)
-  end
+  wait_for_app
 end
 
 Given(/^I am looking at the (Touch|Pan|Rotate\/Pinch|Misc|Tao) tab$/) do |tabname|
