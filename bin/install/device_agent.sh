3ad1d2b3618970ceb0dfb04e52fcca6dbb105081
Christopher Fuentes <chris.fuentes@xamarin.com>
2016-06-10 11:00:58 -0700

    wrap vars in {}

--- bin/install/device_agent.sh
+++ bin/install/device_agent.sh
@@ -9,9 +9,9 @@ SIM_AGENT_HOME="${HOME}/.calabash/DeviceAgent/simulator"
 DEV_AGENT_HOME="${HOME}/.calabash/DeviceAgent/device"  
 
 rm -rf "${HOME}/.calabash/DeviceAgent"
-mkdir -p "$SIM_AGENT_HOME"
-mkdir -p "$DEV_AGENT_HOME"
+mkdir -p "${SIM_AGENT_HOME}"
+mkdir -p "${DEV_AGENT_HOME}"
 
-mv "Products/app/DeviceAgent/CBX-Runner.app" "$SIM_AGENT_HOME"
-mv "Products/ipa/DeviceAgent/CBX-Runner.app" "$DEV_AGENT_HOME"
+mv "Products/app/DeviceAgent/CBX-Runner.app" "${SIM_AGENT_HOME}"
+mv "Products/ipa/DeviceAgent/CBX-Runner.app" "${DEV_AGENT_HOME}"
 
