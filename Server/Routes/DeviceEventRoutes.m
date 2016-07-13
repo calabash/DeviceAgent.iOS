dd8a46525d869353f18a85d20d48337596104d8c
Christopher Fuentes <chris.fuentes@xamarin.com>
2016-07-13 10:21:55 -0700

    rotate_home_button_to: Use long long long value

--- Server/Routes/DeviceEventRoutes.m
+++ Server/Routes/DeviceEventRoutes.m
@@ -53,7 +53,7 @@
                  }];
              }],
              [CBXRoute post:endpoint(@"/rotate_home_button_to", 1.0) withBlock:^(RouteRequest *request, NSDictionary *data, RouteResponse *response) {
-                 [XCUIDevice sharedDevice].orientation = [JSONUtils parseOrientation:data[@"orientation"]];
+                 [XCUIDevice sharedDevice].orientation = [data[@"orientation"] longLongValue];
                  XCUIApplication *app = [Application currentApplication];
                  [response respondWithJSON:@{
                                              @"status" : @"success",
