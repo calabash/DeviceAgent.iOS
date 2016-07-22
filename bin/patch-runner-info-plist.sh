29b56373d3c1c4bd7e978baa5e3a660cc1b977a6
Joshua Moody <joshuajmoody@gmail.com>
2016-07-22 15:01:48 +0200

    Make: fix resigning in info plist patch

--- bin/patch-runner-info-plist.sh
+++ bin/patch-runner-info-plist.sh
@@ -20,10 +20,22 @@ if [ $# == 0 ]; then
 fi
 
 RUNNER="${1}"
+APPSTUB="${1}/../CBXAppStub.app"
 
 if [ ! -e "${RUNNER}" ]; then
-  error "Runner does not exit: ${RUNNER}"
+  error "Runner does not exist: ${RUNNER}"
   exit 1
+fi
+
+if [ ! -e "${APPSTUB}" ]; then
+  error "CBXAppStub does not exist: ${APPSTUB}"
+  exit 1
+fi
+
+if [ "${TERM}" = "dumb" ]; then
+  XCODE_UI_BUILD=1
+else
+  XCODE_UI_BUILD=0
 fi
 
 banner "Patching $(basename ${RUNNER})/Info.plist"
@@ -55,14 +67,69 @@ set -e
 
 banner "Resigning"
 
-DETAILS=`xcrun codesign --display --verbose=3 ${RUNNER} 2>&1`
-CODE_SIGN_IDENTITY=`echo ${DETAILS} | egrep -o "iPhone Developer: .*\)" |  tr -d '\n'`
+# $1 variable to store the identity in
+# $2 the .app to extract the identity from
+# extract_identity IDENTITY "${RUNNER}"
+function extract_identity {
+    DETAILS=`xcrun codesign --display --verbose=3 ${2} 2>&1`
+    TMP=`echo ${DETAILS} | egrep -o "iPhone Developer: .*\)" |  tr -d '\n'`
+    eval "$1=\"${TMP}\""
+}
 
-if [ "${CODE_SIGN_IDENTITY}" = "" ]; then
-  CODE_SIGN_IDENTITY="Mac Developer"
-  info "Will resign with default identity: ${CODE_SIGN_IDENTITY}"
+IDENTITY=""
+
+if [ "${XCODE_UI_BUILD}" = 0 ]; then
+  info "Detected a command line build"
+
+  if [ -n "${CODE_SIGN_IDENTITY}" ]; then
+    info "CODE_SIGN_IDENTITY is set - will use ${CODE_SIGN_IDENTITY} to resign"
+    IDENTITY="${CODE_SIGN_IDENTITY}"
+  else
+    extract_identity IDENTITY "${RUNNER}"
+    info "Will resign with original identity: ${IDENTITY}"
+  fi
 else
-  info "Will resign with original identity: ${CODE_SIGN_IDENTITY}"
+  info "Detected a build from Xcode UI"
+
+  # This is the default value.  It cannot be used because it might generate
+  # 'ambigious match' errors when signing.
+  if [ "${CODE_SIGN_IDENTITY}" = "iPhone Developer" ]; then
+
+    # Try to extract the identity from the CBX-Runner.
+    extract_identity IDENTITY "${RUNNER}"
+
+    # Extracted an identity from the CBX-Runner
+    if [ "${IDENTITY}" != "" ]; then
+      info "Will resign with original identity: ${IDENTITY}"
+    else
+
+      # Typically, the runner has no identity and a bad bundle identifier.
+      #
+      # Identifier=com.apple.test.WRAPPEDPRODUCTNAME-Runner
+      # Authority=Software Signing
+      # Authority=Apple Code Signing Certification Authority
+      # Authority=Apple Root CA
+      info "Runner was not signed with a valid identity"
+      info "Checking CBXAppStub"
+
+      extract_identity IDENTITY "${APPSTUB}"
+
+      # Extracted an identity form the CBXAppStub
+      if [ "${IDENTITY}" != "" ]; then
+        info "Will resign with CBXAppStub identity: ${IDENTITY}"
+      else
+        info "CBXAppStub was not signed with an iPhone Developer identity"
+
+        IDENTITY="iPhone Developer"
+        info "Will resign with default identity: ${IDENTITY}"
+      fi
+    fi
+  else
+
+    # User probably updated the Xcode project with a specific identity
+    info "CODE_SIGN_IDENTITY is set - will use ${CODE_SIGN_IDENTITY} to resign"
+    IDENTITY="${CODE_SIGN_IDENTITY}"
+  fi
 fi
 
 echo ""
@@ -71,7 +138,7 @@ echo ""
 # --deep because the embedded CBX.xctest binary needs to be signed
 # Preserve metadata because we don't want to have to provide a provisioning profile
 xcrun codesign \
-  --sign "${CODE_SIGN_IDENTITY}" \
+  --sign "${IDENTITY}" \
   --force \
   --deep \
   --preserve-metadata=identifier,entitlements,requirements \
