2e2981d26fb60dac8d922af80ddae5ebaa07ad89
Joshua Moody <joshuajmoody@gmail.com>
2016-08-01 17:20:40 +0200

    UITest: add rotate example to show simulator bug
    Xcode 8.0b3

--- TestApp/UITests/UITest.m
+++ TestApp/UITests/UITest.m
@@ -1,11 +1,3 @@
-//
-//  UITest.m
-//  UITest
-//
-//  Created by Joshua Moody on 1.8.16.
-//  Copyright © 2016 Calabash. All rights reserved.
-//
-
 #import <XCTest/XCTest.h>
 
 @interface UITest : XCTestCase
@@ -17,24 +9,24 @@
 - (void)setUp {
     [super setUp];
     
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-    
-    // In UI tests it is usually best to stop immediately when a failure occurs.
     self.continueAfterFailure = NO;
-    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
     [[[XCUIApplication alloc] init] launch];
-    
-    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
-- (void)testExample {
-    // Use recording to get started writing UI tests.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)testRotate {
+    [XCUIDevice sharedDevice].orientation = UIInterfaceOrientationLandscapeLeft;
+//    UIDeviceOrientation deviceOrientation = [XCUIDevice sharedDevice].orientation;
+//    NSLog(@"device orientation = %@", @(deviceOrientation));
+//
+//    UIInterfaceOrientation interfaceOrientation = [[UIApplication sharedApplication] statusBarOrientation];
+//    NSLog(@"interface orientation = %@", @(interfaceOrientation));
+//
+//    XCTAssertEqual([@(deviceOrientation) integerValue],
+//                   [@(interfaceOrientation) integerValue]);
 }
 
 @end
