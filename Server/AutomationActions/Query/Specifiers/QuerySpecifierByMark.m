5c9b7f930a159e01176f9fb774eac6eed108c9c2
Joshua Moody <joshuajmoody@gmail.com>
2016-09-04 20:49:58 +0200

    Server: add ByMark query specifier

---
+++ Server/AutomationActions/Query/Specifiers/QuerySpecifierByMark.m
@@ -0,0 +1,55 @@
+
+#import "QuerySpecifierByMark.h"
+#import "XCUIElementQuery.h"
+
+@implementation QuerySpecifierByMark
+
+static NSArray<NSString *> *markedProperties;
+
++ (void)load {
+    static dispatch_once_t once_token;
+    dispatch_once(&once_token, ^{
+        markedProperties = @[
+                             @"identifier",
+                             @"accessibilityIdentifier",
+                             @"label",
+                             @"accessibilityLabel",
+                             @"title",
+                             @"value",
+                             @"placeholderValue"
+                             ];
+    });
+}
+
++ (NSString *)name { return @"marked"; }
+
+- (XCUIElementQuery *)applyInternal:(XCUIElementQuery *)query {
+    /*
+     TODO: Performance win with NSCompoundPredicate?
+
+     JJM: I tried NSCompoundPredicate and I consistently got no results.  My best
+     guess is that XCUIElementQuery does not respond to compound predicates.
+
+     We could try NSPredicate predicateWithBlock:, but my guess is that this will
+     not work.
+
+     Recall that CoreData does not allow block predicates, so there is some
+     precedence for restricting NSPredicate kinds (?) in certain contexts.
+
+     NOTE: Using == here is appropriate.  LIKE responds to the ? character which
+     will be a problem if the value contains a ?.
+     */
+
+    NSString *escaped = [QuerySpecifier escapeString:self.value];
+    NSMutableString *predicateString = [NSMutableString string];
+    for (NSString *prop in markedProperties) {
+        [predicateString appendFormat:@"%@ == '%@'", prop, escaped];
+        if (prop != [markedProperties lastObject]) {
+            [predicateString appendString:@" OR "];
+        }
+    }
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:predicateString];
+    return [query matchingPredicate:predicate];
+}
+
+@end
