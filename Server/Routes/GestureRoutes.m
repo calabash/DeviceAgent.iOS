7c96301241a8e14d6971d2ac7aef3fd838777457
Joshua Moody <joshuajmoody@gmail.com>
2016-09-09 05:52:42 +0200

    Server: dismiss SB alerts before /gesture

--- Server/Routes/GestureRoutes.m
+++ Server/Routes/GestureRoutes.m
@@ -9,20 +9,24 @@
 #import "Application.h"
 #import "JSONUtils.h"
 #import "CBXMacros.h"
+#import "SpringBoard.h"
 
 @implementation GestureRoutes
 + (NSArray<CBXRoute *> *)getRoutes {
     return @[
              [CBXRoute post:endpoint(@"/gesture", 1.0) withBlock:^(RouteRequest *request, NSDictionary *body, RouteResponse *response) {
-                 [GestureFactory executeGestureWithJSON:body
-                                               completion:^(NSError *e) {
-                       if (e) {
-                           [response respondWithJSON:@{ @"error" : e.localizedDescription }]; //should never execute
-                       } else {
-                           [response respondWithJSON:@{ @"status" : @"success" }];
-                       }
+                 [[SpringBoard application] handleAlertsOrThrow];
+
+                 [GestureFactory executeGestureWithJSON:body completion:^(NSError *e) {
+                     if (e) {
+                         //should never execute
+                         [response respondWithJSON:@{ @"error" : e.localizedDescription }];
+                     } else {
+                         [response respondWithJSON:@{ @"status" : @"success" }];
+                     }
                  }];
              }],
+
              [CBXRoute post:endpoint(@"/gesture/:test_id", 1.0) withBlock:^(RouteRequest *request, NSDictionary *body, RouteResponse *response) {
                  NSNumber *identifier = @([request.params[CBX_TEST_ID_KEY] integerValue]);
                  XCUIElement *el = [Application cachedElementOrThrow:identifier];
