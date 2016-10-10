11c98cf4be3a5bdd6be07c1ec89a25175b1bfc13
Joshua Moody <joshuajmoody@gmail.com>
2016-10-10 18:17:26 +0200

    WIP: clear text touches delete key

--- Server/AutomationActions/Gestures/ClearText.m
+++ Server/AutomationActions/Gestures/ClearText.m
@@ -13,38 +13,35 @@
                                        query:(Query *)query
                                   completion:(CompletionBlock)completion {
 
-    QueryConfiguration *config = [QueryConfiguration withJSON:@{
-                                                                @"property" : @"hasKeyboardFocus=YES"
-                                                                }
-                                                    validator:nil];
+    QueryConfiguration *config =
+    [QueryConfiguration withJSON:@{
+                                   @"property" : @"hasKeyboardFocus=YES"
+                                   }
+                       validator:nil];
     Query *focusedElementQuery = [Query withQueryConfiguration:config];
     NSArray *results = [focusedElementQuery execute];
+
     if (results.count == 0) {
         @throw [CBXException withMessage:@"Can not clear text: no element has focus"];
     } else if (results.count > 1) {
         @throw [CBXException withMessage:@"Refusing to clear text: multiple elements have focus"];
     }
-    XCUIElement *el = results[0];
 
-    __block NSError *err;
-    [ThreadUtils runSync:^(BOOL *setToTrueWhenDone) {
-        NSString *string = el.value;
-        NSMutableString *delString = [NSMutableString new];
-        for (int i = 0; i < string.length; i++) {
-            [delString appendString:@"\b"];
+    XCUIElement *elementWithFocus = results[0];
+    NSString *string = elementWithFocus.value;
+
+    if (!string || [string length] == 0) {
+        completion(nil);
+        return nil;
+    } else {
+        for (NSUInteger index = 0; index < [string length]; index++) {
+          // touch the delete key
+          // In the run-loop client I use `marked:'delete'`
+
         }
-        [el typeText:delString];
-
-        [[Testmanagerd get] _XCT_sendString:string
-                           maximumFrequency:CBX_DEFAULT_SEND_STRING_FREQUENCY
-                                 completion:^(NSError *e) {
-                                     err = e;
-                                     *setToTrueWhenDone = YES;
-                                 }];
-    }];
-    
-    completion(err);
-    return nil;
+        completion(nil);
+        return nil;
+    }
 }
 
 @end
