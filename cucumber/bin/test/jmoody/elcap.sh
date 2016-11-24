17cc0ce4d2e6f43a7d9c403c10a7b9dd2e280f0f
Joshua Moody <joshuajmoody@gmail.com>
2016-11-24 17:21:19 +0100

    Test: jmoody test scripts can take sim/device arguments

--- cucumber/bin/test/jmoody/elcap.sh
+++ cucumber/bin/test/jmoody/elcap.sh
@@ -3,6 +3,20 @@
 set -e
 CUCUMBER_ARGS="--format progress --tags @keyboard"
 
+TEST_SIM=1
+TEST_DEVICE=1
+
+if [ "${1}" = "device" ]; then
+  TEST_SIM=0
+  TEST_DEVICE=1
+elif [ "${1}" = "sim" ]; then
+  TEST_SIM=1
+  TEST_DEVICE=0
+else
+  echo "Error: unknown argument: ${1}"
+  exit 1
+fi
+
 export CODE_SIGN_IDENTITY="iPhone Developer: Karl Krukow (YTTN6Y2QS9)"
 
 # Quit the TestApp at_exit to ensure the next test launches the app
@@ -39,31 +53,35 @@ export DEVELOPER_DIR=/Xcode/8.0/Xcode.app/Contents/Developer
 banner "Xcode 8.0"
 xcrun xcodebuild -version
 
-info "Default Simulator"
-bundle exec cucumber ${CUCUMBER_ARGS}
-
-info "iOS 9.3 Simulator"
-DEVICE_TARGET=BD2010F9-401C-4E56-AE8A-ECB7CD3370D8 \
+if [ $TEST_SIM -eq 1 ]; then
+  info "Default Simulator"
   bundle exec cucumber ${CUCUMBER_ARGS}
 
-info "iOS 9.3 Simulator iPhone 5c"
-DEVICE_TARGET=D1B22B9C-F105-4DF0-8FA3-7AE41E212A9D \
-  bundle exec cucumber ${CUCUMBER_ARGS}
-
-info "iOS 10.0 Simulator iPhone 5c"
-DEVICE_TARGET=FEBB7F64-516F-46B5-9CD8-BC361A297A34 \
-  bundle exec cucumber ${CUCUMBER_ARGS}
-
-declare -a devices=("mercury" "wolf" "uranus")
-
-for device in "${devices[@]}"
-do
-  info "Device: $device"
-
-  DEVICE_TARGET=$device \
-    APP=sh.calaba.TestApp \
+  info "iOS 9.3 Simulator"
+  DEVICE_TARGET=BD2010F9-401C-4E56-AE8A-ECB7CD3370D8 \
     bundle exec cucumber ${CUCUMBER_ARGS}
-done
+
+  info "iOS 9.3 Simulator iPhone 5c"
+  DEVICE_TARGET=D1B22B9C-F105-4DF0-8FA3-7AE41E212A9D \
+    bundle exec cucumber ${CUCUMBER_ARGS}
+
+  info "iOS 10.0 Simulator iPhone 5c"
+  DEVICE_TARGET=FEBB7F64-516F-46B5-9CD8-BC361A297A34 \
+    bundle exec cucumber ${CUCUMBER_ARGS}
+fi
+
+if [ $TEST_DEVICE -eq 1 ]; then
+  declare -a devices=("mercury" "wolf" "uranus")
+
+  for device in "${devices[@]}"
+  do
+    info "Device: $device"
+
+    DEVICE_TARGET=$device \
+      APP=sh.calaba.TestApp \
+      bundle exec cucumber ${CUCUMBER_ARGS}
+  done
+fi
 
 # Xcode 8.1
 export DEVELOPER_DIR=/Xcode/8.1/Xcode.app/Contents/Developer
@@ -71,38 +89,42 @@ export DEVELOPER_DIR=/Xcode/8.1/Xcode.app/Contents/Developer
 banner "Xcode 8.1"
 xcrun xcodebuild -version
 
-info "Default Simulator"
-bundle exec cucumber ${CUCUMBER_ARGS}
-
-info "iOS 10.0 Simulator"
-DEVICE_TARGET=B35DFE6A-3675-4154-912D-2C6864AD4AAF \
+if [ $TEST_SIM -eq 1 ]; then
+  info "Default Simulator"
   bundle exec cucumber ${CUCUMBER_ARGS}
 
-info "iOS 9.3 Simlator"
-DEVICE_TARGET=F6EF1FA5-1C3F-465E-8B29-70C293DE0F66 \
-  bundle exec cucumber ${CUCUMBER_ARGS}
-
-info "iOS 9.3 Simulator iPhone 5c"
-DEVICE_TARGET=D1B22B9C-F105-4DF0-8FA3-7AE41E212A9D \
-  bundle exec cucumber ${CUCUMBER_ARGS}
-
-info "iOS 10.0 Simulator iPhone 5c"
-DEVICE_TARGET=FEBB7F64-516F-46B5-9CD8-BC361A297A34 \
-  bundle exec cucumber ${CUCUMBER_ARGS}
-
-info "iOS 10.1 Simulator iPhone 5c"
-DEVICE_TARGET=E0D0814E-E655-482C-A10A-3019E3A0BCE7 \
-  bundle exec cucumber ${CUCUMBER_ARGS}
-
-declare -a devices=("mercury" "wolf" "uranus" "pegasi" "denis")
-
-for device in "${devices[@]}"
-do
-  info "Device: $device"
-  DEVICE_TARGET=$device \
-    APP=sh.calaba.TestApp \
+  info "iOS 10.0 Simulator"
+  DEVICE_TARGET=B35DFE6A-3675-4154-912D-2C6864AD4AAF \
     bundle exec cucumber ${CUCUMBER_ARGS}
-done
+
+  info "iOS 9.3 Simlator"
+  DEVICE_TARGET=F6EF1FA5-1C3F-465E-8B29-70C293DE0F66 \
+    bundle exec cucumber ${CUCUMBER_ARGS}
+
+  info "iOS 9.3 Simulator iPhone 5c"
+  DEVICE_TARGET=D1B22B9C-F105-4DF0-8FA3-7AE41E212A9D \
+    bundle exec cucumber ${CUCUMBER_ARGS}
+
+  info "iOS 10.0 Simulator iPhone 5c"
+  DEVICE_TARGET=FEBB7F64-516F-46B5-9CD8-BC361A297A34 \
+    bundle exec cucumber ${CUCUMBER_ARGS}
+
+  info "iOS 10.1 Simulator iPhone 5c"
+  DEVICE_TARGET=E0D0814E-E655-482C-A10A-3019E3A0BCE7 \
+    bundle exec cucumber ${CUCUMBER_ARGS}
+fi
+
+if [ $TEST_DEVICE -eq 1 ]; then
+  declare -a devices=("mercury" "wolf" "uranus" "pegasi" "denis")
+
+  for device in "${devices[@]}"
+  do
+    info "Device: $device"
+    DEVICE_TARGET=$device \
+      APP=sh.calaba.TestApp \
+      bundle exec cucumber ${CUCUMBER_ARGS}
+  done
+fi
 
 # Xcode 8.2 beta 2
 export DEVELOPER_DIR=/Xcode/8.2/Xcode-beta.app/Contents/Developer
@@ -113,41 +135,45 @@ xcrun xcodebuild -version
 info "Default Simulator"
 bundle exec cucumber ${CUCUMBER_ARGS}
 
-info "iOS 10.1 Simulator"
-DEVICE_TARGET= 8B9B8DE5-79DA-4C2E-B430-0FD0FC4FE58D \
-  bundle exec cucumber ${CUCUMBER_ARGS}
+if [ $TEST_SIM -eq 1 ]; then
+  info "iOS 10.1 Simulator"
+  DEVICE_TARGET=8B9B8DE5-79DA-4C2E-B430-0FD0FC4FE58D \
+    bundle exec cucumber ${CUCUMBER_ARGS}
 
-info "iOS 10.0 Simlator"
-DEVICE_TARGET=B35DFE6A-3675-4154-912D-2C6864AD4AAF \
-  bundle exec cucumber ${CUCUMBER_ARGS}
+  info "iOS 10.0 Simlator"
+  DEVICE_TARGET=B35DFE6A-3675-4154-912D-2C6864AD4AAF \
+    bundle exec cucumber ${CUCUMBER_ARGS}
 
-info "iOS 9.3 Simulator"
-DEVICE_TARGET=F6EF1FA5-1C3F-465E-8B29-70C293DE0F66 \
-  bundle exec cucumber ${CUCUMBER_ARGS}
+  info "iOS 9.3 Simulator"
+  DEVICE_TARGET=F6EF1FA5-1C3F-465E-8B29-70C293DE0F66 \
+    bundle exec cucumber ${CUCUMBER_ARGS}
 
-info "iOS 9.3 Simulator iPhone 5c"
-DEVICE_TARGET=D1B22B9C-F105-4DF0-8FA3-7AE41E212A9D \
-  bundle exec cucumber ${CUCUMBER_ARGS}
+  info "iOS 9.3 Simulator iPhone 5c"
+  DEVICE_TARGET=D1B22B9C-F105-4DF0-8FA3-7AE41E212A9D \
+    bundle exec cucumber ${CUCUMBER_ARGS}
 
-info "iOS 10.0 Simulator iPhone 5c"
-DEVICE_TARGET=FEBB7F64-516F-46B5-9CD8-BC361A297A34 \
-  bundle exec cucumber ${CUCUMBER_ARGS}
+  info "iOS 10.0 Simulator iPhone 5c"
+  DEVICE_TARGET=FEBB7F64-516F-46B5-9CD8-BC361A297A34 \
+    bundle exec cucumber ${CUCUMBER_ARGS}
 
-info "iOS 10.1 Simulator iPhone 5c"
-DEVICE_TARGET=E0D0814E-E655-482C-A10A-3019E3A0BCE7 \
-  bundle exec cucumber ${CUCUMBER_ARGS}
+  info "iOS 10.1 Simulator iPhone 5c"
+  DEVICE_TARGET=E0D0814E-E655-482C-A10A-3019E3A0BCE7 \
+    bundle exec cucumber ${CUCUMBER_ARGS}
 
-info "iOS 10.2 Simulator iPhone 5c"
-DEVICE_TARGET=8B4F2F90-FEAB-4014-8B20-419ED7D63DD5 \
-  bundle exec cucumber ${CUCUMBER_ARGS}
+  info "iOS 10.2 Simulator iPhone 5c"
+  DEVICE_TARGET=8B4F2F90-FEAB-4014-8B20-419ED7D63DD5 \
+    bundle exec cucumber ${CUCUMBER_ARGS}
+fi
 
 declare -a devices=("mercury" "wolf" "uranus" "pegasi" "denis" "hat")
 
-for device in "${devices[@]}"
-do
-  info "Device: $device"
-  DEVICE_TARGET=$device \
-    APP=sh.calaba.TestApp \
-    bundle exec cucumber ${CUCUMBER_ARGS}
-done
+if [ $TEST_DEVICE -eq 1 ]; then
+  for device in "${devices[@]}"
+  do
+    info "Device: $device"
+    DEVICE_TARGET=$device \
+      APP=sh.calaba.TestApp \
+      bundle exec cucumber ${CUCUMBER_ARGS}
+  done
+if
 
--- cucumber/bin/test/jmoody/sierra.sh
+++ cucumber/bin/test/jmoody/sierra.sh
@@ -3,6 +3,20 @@
 set -e
 CUCUMBER_ARGS="--format progress --tags @keyboard"
 
+TEST_SIM=1
+TEST_DEVICE=1
+
+if [ "${1}" = "device" ]; then
+  TEST_SIM=0
+  TEST_DEVICE=1
+elif [ "${1}" = "sim" ]; then
+  TEST_SIM=1
+  TEST_DEVICE=0
+else
+  echo "Error: unknown argument: ${1}"
+  exit 1
+fi
+
 export CODE_SIGN_IDENTITY="iPhone Developer: Karl Krukow (YTTN6Y2QS9)"
 
 # Quit the TestApp at_exit to ensure the next test launches the app
@@ -39,23 +53,35 @@ export DEVELOPER_DIR=/Xcode/8.0/Xcode.app/Contents/Developer
 banner "Xcode 8.0"
 xcrun xcodebuild -version
 
-info "Default Simulator"
-bundle exec cucumber ${CUCUMBER_ARGS}
-
-info "iOS 9.3 Simulator"
-DEVICE_TARGET=7D25AA5B-F939-4BC8-AD5D-6280E71DAE0B \
+if [ $TEST_SIM -eq 1 ]; then
+  info "Default Simulator"
   bundle exec cucumber ${CUCUMBER_ARGS}
 
-declare -a devices=("mercury" "wolf" "uranus")
-
-for device in "${devices[@]}"
-do
-  info "Device: $device"
-
-  DEVICE_TARGET=$device \
-    APP=sh.calaba.TestApp \
+  info "iOS 9.3 Simulator"
+  DEVICE_TARGET=7D25AA5B-F939-4BC8-AD5D-6280E71DAE0B \
     bundle exec cucumber ${CUCUMBER_ARGS}
-done
+
+  info "iOS 9.3 Simulator iPhone 5c"
+  DEVICE_TARGET=3BFEF174-BBF3-48C0-B860-7898250F2254 \
+    bundle exec cucumber ${CUCUMBER_ARGS}
+
+  info "iOS 10.0 Simulator iPhone 5c"
+  DEVICE_TARGET=42EF5066-9946-498C-A684-9C8AF702DEC3 \
+    bundle exec cucumber ${CUCUMBER_ARGS}
+fi
+
+if [ $TEST_DEVICE -eq 1 ]; then
+  declare -a devices=("mercury" "wolf" "uranus")
+
+  for device in "${devices[@]}"
+  do
+    info "Device: $device"
+
+    DEVICE_TARGET=$device \
+      APP=sh.calaba.TestApp \
+      bundle exec cucumber ${CUCUMBER_ARGS}
+  done
+fi
 
 # Xcode 8.1
 export DEVELOPER_DIR=/Xcode/8.1/Xcode.app/Contents/Developer
@@ -63,26 +89,42 @@ export DEVELOPER_DIR=/Xcode/8.1/Xcode.app/Contents/Developer
 banner "Xcode 8.1"
 xcrun xcodebuild -version
 
-info "Default Simulator"
-bundle exec cucumber ${CUCUMBER_ARGS}
-
-info "iOS 10.0 Simulator"
-DEVICE_TARGET=B6D21623-32F6-44F5-90ED-7E520DC94A69 \
+if [ $TEST_SIM -eq 1 ]; then
+  info "Default Simulator"
   bundle exec cucumber ${CUCUMBER_ARGS}
 
-info "iOS 9.3 Simlator"
-DEVICE_TARGET=6DD279B3-7FED-4902-96F4-618D55540ADC \
-  bundle exec cucumber ${CUCUMBER_ARGS}
-
-declare -a devices=("mercury" "wolf" "uranus" "pegasi" "denis")
-
-for device in "${devices[@]}"
-do
-  info "Device: $device"
-  DEVICE_TARGET=$device \
-    APP=sh.calaba.TestApp \
+  info "iOS 10.0 Simulator"
+  DEVICE_TARGET=B6D21623-32F6-44F5-90ED-7E520DC94A69 \
     bundle exec cucumber ${CUCUMBER_ARGS}
-done
+
+  info "iOS 9.3 Simlator"
+  DEVICE_TARGET=6DD279B3-7FED-4902-96F4-618D55540ADC \
+    bundle exec cucumber ${CUCUMBER_ARGS}
+
+  info "iOS 9.3 Simulator iPhone 5c"
+  DEVICE_TARGET=3BFEF174-BBF3-48C0-B860-7898250F2254 \
+    bundle exec cucumber ${CUCUMBER_ARGS}
+
+  info "iOS 10.0 Simulator iPhone 5c"
+  DEVICE_TARGET=42EF5066-9946-498C-A684-9C8AF702DEC3 \
+    bundle exec cucumber ${CUCUMBER_ARGS}
+
+  info "iOS 10.1 Simulator iPhone 5c"
+  DEVICE_TARGET=659F0339-A6A9-460E-BFDD-30E7F12F7E7C \
+    bundle exec cucumber ${CUCUMBER_ARGS}
+fi
+
+if [ $TEST_DEVICE -eq 1 ]; then
+  declare -a devices=("mercury" "wolf" "uranus" "pegasi" "denis")
+
+  for device in "${devices[@]}"
+  do
+    info "Device: $device"
+    DEVICE_TARGET=$device \
+      APP=sh.calaba.TestApp \
+      bundle exec cucumber ${CUCUMBER_ARGS}
+  done
+fi
 
 # Xcode 8.2 beta 2
 export DEVELOPER_DIR=/Xcode/8.2/Xcode-beta.app/Contents/Developer
@@ -90,28 +132,48 @@ export DEVELOPER_DIR=/Xcode/8.2/Xcode-beta.app/Contents/Developer
 banner "Xcode 8.2"
 xcrun xcodebuild -version
 
-info "Default Simulator"
-bundle exec cucumber ${CUCUMBER_ARGS}
-
-info "iOS 10.1 Simulator"
-DEVICE_TARGET=E160BC4E-CAD5-4680-9C42-D510FB4BB052 \
+if [ $TEST_SIM -eq 1 ]; then
+  info "Default Simulator"
   bundle exec cucumber ${CUCUMBER_ARGS}
 
-info "iOS 10.0 Simlator"
-DEVICE_TARGET=6DD279B3-7FED-4902-96F4-618D55540ADC \
-  bundle exec cucumber ${CUCUMBER_ARGS}
-
-info "iOS 9.3 Simulator"
-DEVICE_TARGET=B6D21623-32F6-44F5-90ED-7E520DC94A69 \
-  bundle exec cucumber ${CUCUMBER_ARGS}
-
-declare -a devices=("mercury" "wolf" "uranus" "pegasi" "denis" "hat")
-
-for device in "${devices[@]}"
-do
-  info "Device: $device"
-  DEVICE_TARGET=$device \
-    APP=sh.calaba.TestApp \
+  info "iOS 10.1 Simulator"
+  DEVICE_TARGET=E160BC4E-CAD5-4680-9C42-D510FB4BB052 \
     bundle exec cucumber ${CUCUMBER_ARGS}
-done
+
+  info "iOS 10.0 Simlator"
+  DEVICE_TARGET=6DD279B3-7FED-4902-96F4-618D55540ADC \
+    bundle exec cucumber ${CUCUMBER_ARGS}
+
+  info "iOS 9.3 Simulator"
+  DEVICE_TARGET=B6D21623-32F6-44F5-90ED-7E520DC94A69 \
+    bundle exec cucumber ${CUCUMBER_ARGS}
+
+  info "iOS 9.3 Simulator iPhone 5c"
+  DEVICE_TARGET=3BFEF174-BBF3-48C0-B860-7898250F2254 \
+    bundle exec cucumber ${CUCUMBER_ARGS}
+
+  info "iOS 10.0 Simulator iPhone 5c"
+  DEVICE_TARGET=42EF5066-9946-498C-A684-9C8AF702DEC3 \
+    bundle exec cucumber ${CUCUMBER_ARGS}
+
+  info "iOS 10.1 Simulator iPhone 5c"
+  DEVICE_TARGET=659F0339-A6A9-460E-BFDD-30E7F12F7E7C \
+    bundle exec cucumber ${CUCUMBER_ARGS}
+
+  info "iOS 10.2 Simulator iPhone 5c"
+  DEVICE_TARGET=83B48D5E-A97B-4CE0-8B43-AD8B841303A7 \
+    bundle exec cucumber ${CUCUMBER_ARGS}
+fi
+
+if [ $TEST_DEVICE -eq 1 ]; then
+  declare -a devices=("mercury" "wolf" "uranus" "pegasi" "denis" "hat")
+
+  for device in "${devices[@]}"
+  do
+    info "Device: $device"
+    DEVICE_TARGET=$device \
+      APP=sh.calaba.TestApp \
+      bundle exec cucumber ${CUCUMBER_ARGS}
+  done
+fi
 
