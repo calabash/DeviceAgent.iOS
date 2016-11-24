697d26cfa4afd3f54289be240422738eb3793eca
Joshua Moody <joshuajmoody@gmail.com>
2016-11-24 09:04:33 +0100

    EnterText: use typeText: on non-arm64 arches

--- Server/AutomationActions/Gestures/EnterText.m
+++ Server/AutomationActions/Gestures/EnterText.m
@@ -3,7 +3,8 @@
 #import "Testmanagerd.h"
 #import "ThreadUtils.h"
 #import "EnterText.h"
-#import "Application.h"
+#import "CBXDevice.h"
+#import "TextInputFirstResponderProvider.h"
 
 @implementation EnterText
 
@@ -17,6 +18,7 @@
     return @[];
 }
 
+// Callers are responsible for checking that a keyboard is visible.
 + (Gesture *)executeWithGestureConfiguration:(GestureConfiguration *)gestureConfig
                                        query:(Query *)query
                                   completion:(CompletionBlock)completion {
@@ -27,18 +29,36 @@
     NSString *string = gestureConfig[CBX_STRING_KEY];
 
     __block NSError *outerError = nil;
-    [ThreadUtils runSync:^(BOOL *setToTrueWhenDone) {
-        [[Testmanagerd get] _XCT_sendString:string
-                           maximumFrequency:CBX_DEFAULT_SEND_STRING_FREQUENCY
-                                 completion:^(NSError *innerError) {
-                                     outerError = innerError;
-                                     *setToTrueWhenDone = YES;
-                                     if (innerError) {
-                                         @throw [CBXException withMessage:@"Error performing gesture"];
-                                     }
-                                 }];
-    }];
-    
+    if ([[CBXDevice sharedDevice] isArm64]) {
+        NSLog(@"Device has arm64 architecture: %@", [[CBXDevice sharedDevice] armVersion]);
+        [ThreadUtils runSync:^(BOOL *setToTrueWhenDone) {
+            [[Testmanagerd get]
+             _XCT_sendString:string
+             maximumFrequency:CBX_DEFAULT_SEND_STRING_FREQUENCY
+             completion:^(NSError *innerError) {
+                 outerError = innerError;
+                 *setToTrueWhenDone = YES;
+                 NSString *message;
+                 message = [NSString stringWithFormat:@"Encountered an error typing text: %@",
+                            innerError];
+                 if (innerError) {
+                     @throw [CBXException withMessage:message];
+                 }
+             }];
+        }];
+    } else {
+        NSLog(@"Device does not have arm64 architecture: %@",
+              [[CBXDevice sharedDevice] armVersion]);
+        // Gestures need to be automatic - a gesture must complete before the next
+        // gesture started.  Depending on the internal implementation of typeText:,
+        // this should block other gestures.
+        @synchronized([Testmanagerd get]) {
+            TextInputFirstResponderProvider *provider = [TextInputFirstResponderProvider new];
+            XCUIElement *firstResponder = [provider firstResponderOrApplication];
+            [firstResponder typeText:string];
+        }
+    }
+
     completion(outerError);
     return nil;
 }
