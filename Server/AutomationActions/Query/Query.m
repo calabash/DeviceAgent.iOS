186d6f0d133de2cc56170a5446dd551599f42844
Joshua Moody <joshuajmoody@gmail.com>
2017-06-22 09:28:31 +0200

    Server: replace == nil checks with not operator

--- Server/AutomationActions/Query/Query.m
+++ Server/AutomationActions/Query/Query.m
@@ -34,15 +34,11 @@
         @throw [CBXException withMessage:@"Query must have at least one specifier"];
     }
 
-    if ([Application currentApplication] == nil) {
-        @throw [CBXException withMessage:@"Can not perform queries until application has been launched!"];
+    if (![Application currentApplication]) {
+        @throw [CBXException withMessage:@"Cannot perform queries until application has been launched!"];
     }
 
-    /*
-     Building the XCUIElementQuery
-     */
-
-    if ([[Application currentApplication] lastSnapshot] == nil) {
+    if (![[Application currentApplication] lastSnapshot]) {
         [[[Application currentApplication] applicationQuery] elementBoundByIndex:0];
         [[Application currentApplication] resolve];
     }
