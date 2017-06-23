17ade1ea5d887670e918153d24a0a38f0b58b431
Joshua Moody <joshuajmoody@gmail.com>
2017-06-23 10:00:12 +0200

    Jenkins: do not force Xcode 8.1

--- bin/ci/jenkins.sh
+++ bin/ci/jenkins.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-
-export DEVELOPER_DIR=/Xcode/8.1/Xcode.app/Contents/Developer
-
 xcrun simctl help >/dev/null 2>&1
 xcrun simctl help >/dev/null 2>&1
 xcrun simctl help >/dev/null 2>&1
