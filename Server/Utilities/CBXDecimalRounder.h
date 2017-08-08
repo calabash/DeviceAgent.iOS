12f6366ed79e445982faad410bc8dcd444757cf3
Joshua Moody <joshuajmoody@gmail.com>
2017-08-08 15:45:34 +0200

    Server: add class for rounding floats
    Copied from the LPServer

---
+++ Server/Utilities/CBXDecimalRounder.h
@@ -0,0 +1,34 @@
+
+#import <UIKit/UIKit.h>
+
+/**
+ A class for rounding floats to decimal places.
+ */
+@interface CBXDecimalRounder : NSObject
+
+/**
+ Rounds the float in the argument to 2 decimal places.
+
+ @param cgFloat the float to round
+ @return a CGFloat rounded to 2 decimal places.
+ */
+- (CGFloat)round:(CGFloat)cgFloat;
+
+/**
+ Rounds the cgFloat argument.
+
+ @param cgFloat the float to round
+ @param scale the number of decimal places after rounding.
+ @return a CGFloat round to `scale` decimal places.
+ */
+- (CGFloat)round:(CGFloat)cgFloat withScale:(NSUInteger)scale;
+
+/**
+ Rounds the cgFloat to the nearest integer.
+
+ @param cgFloat the float to round
+ @return an NSInteger
+ */
+- (NSInteger)integerByRounding:(CGFloat)cgFloat;
+
+@end
