b3da379a0abcde176f1abc362a4c81ea47732d65
Joshua Moody <joshuajmoody@gmail.com>
2017-08-09 16:27:14 +0200

    Server: update docs for DeviceEventRoutes

--- Server/Routes/DeviceEventRoutes.h
+++ Server/Routes/DeviceEventRoutes.h
@@ -3,16 +3,20 @@
 #import "CBXProtocols.h"
 
 /**
- Routes relating to raw device events. 
- 
+ Routes relating to raw device events.
+
  ## POST @"/home"
  Press the home button
- 
+
  ## POST @"/siri"
  Hold the home button long enough to invoke Siri
- 
 
-TODO: Orientation
+ ## POST @"/volume"
+ Touch the volume controls
+
+ ## POST @"/rotate_home_button_to"
+ Change the device orientation
+
  */
 @interface DeviceEventRoutes : NSObject<CBRouteProvider>
 @end
