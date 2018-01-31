83988e426f06d671e9ef87133adbbcfb22e11ac0
Joshua Moody <joshuajmoody@gmail.com>
2018-01-31 08:17:25 +0100

    Bin: improvements to xtc.sh submit script

--- bin/xtc.sh
+++ bin/xtc.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+source bin/log.sh
+
 set -e
 
 if [ -z ${1} ]; then
@@ -15,12 +17,18 @@ The DeviceAgent-SHA optional argument allows tests to be run against any
 DeviceAgent that has been uploaded to S3 rather than the current active
 DeviceAgent for Test Cloud.
 
+If you need to test local changes to run-loop or Calabash on Test Cloud,
+use the BUILD_RUN_LOOP and BUILD_CALABASH env variables.
+
 Responds to these env variables:
 
         SERIES: the Test Cloud series
 SKIP_IPA_BUILD: iff 1, then skip re-building the ipa.
                 'make test-ipa' will still be called, so changes in the
                 features/ directory will be staged and sent to Test Cloud.
+BUILD_RUN_LOOP: iff 1, then rebuild run-loop gem before uploading.
+BUILD_RUN_LOOP: iff 1, then rebuild Calabash iOS gem before uploading.
+
 "
 
   exit 64
@@ -43,15 +51,17 @@ fi
 
 source "${CREDS}"
 
-# TODO control with arguments or env variables
-#
 # The uninstall/install dance is required to test changes in
 # run-loop and calabash-cucumber in Test Cloud
-#gem uninstall -Vax --force --no-abort-on-dependent run_loop
-#(cd ../run_loop; rake install)
+if [ "${BUILD_RUN_LOOP}" = "1" ]; then
+  gem uninstall -Vax --force --no-abort-on-dependent run_loop
+  (cd ../run_loop; rake install)
+fi
 
-#gem uninstall -Vax --force --no-abort-on-dependent calabash-cucumber
-#(cd ../calabash-ios/calabash-cucumber; rake install)
+if [ "${BUILD_CALABASH}" = "1" ]; then
+  gem uninstall -Vax --force --no-abort-on-dependent calabash-cucumber
+  (cd ../calabash-ios/calabash-cucumber; rake install)
+fi
 
 PREPARE_XTC_ONLY="${SKIP_IPA_BUILD}" make test-ipa
 
@@ -62,6 +72,7 @@ mkdir -p .xtc
 
 if [ "${2}" != "" ]; then
   echo "${2}" > .xtc/device-agent-sha
+fi
 
 if [ "${SERIES}" = "" ]; then
   SERIES=master
