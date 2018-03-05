9fc84f537e4c14437a0673b68564e1d82af33e8c
Joshua Moody <joshuajmoody@gmail.com>
2018-03-05 06:57:15 +0100

    Doc: disable appledoc script for Xcode 9.3
    docsutils is not available in Xcode 9.3

--- bin/make/docs.sh
+++ bin/make/docs.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 
-function info {
-  echo "$(tput setaf 2)INFO: $1$(tput sgr0)"
-}
+source "bin/log.sh"
+source "bin/xcode.sh"
 
-function warn {
-  echo "$(tput setaf 3)WARN: $1$(tput sgr0)"
-}
-
-function error {
-  echo "$(tput setaf 1)ERROR: $1$(tput sgr0)"
-}
+if [ "$(xcode_gte_93)" = "true" ]; then
+  error "appledoc is broken for Xcode 9.3"
+  exit 1
+fi
 
 # $1 => error code
 function fail_in_ci {
@@ -43,13 +39,16 @@ appledoc \
 --no-repeat-first-par \
 --ignore "*.m" \
 --ignore "Server/NSXPCConnection.h" \
+--ignore "Server/CBXTypedefs.h" \
 --ignore "Server/Testmanagerd/DTXConnectionServices" \
 --ignore "Server/Testmanagerd/DTXConnectionServices.framework" \
 --ignore "Server/Testmanagerd/MobileDevice.framework" \
 --ignore "Server/Testmanagerd/XCUITestManagerDLink" \
 --ignore "Server/Testmanagerd/XCUITestManagerDLink.framework" \
---ignore "Server/XCTest" \
+--ignore "Server/PrivateHeaders" \
 --ignore "Server/FBWebDriverAgent" \
+--ignore "Server/XCUIApplicationStateTypedef.h" \
+--ignore "Server/XCTest+CBXAdditions.h" \
 ./Server
 
 EC=$?
--- bin/xcode.sh
+++ bin/xcode.sh
@@ -17,6 +17,20 @@ function xcode_gte_9 {
   fi
 }
 
+function xcode_gte_93 {
+  if [ "$(xcode_gte_9)" = "false" ]; then
+    echo -n "false"
+  else
+    local version=$(xcode_version)
+    local minor=$(echo $version | cut -d. -f2)
+    if [ "${minor}" \> "3" -o "${minor}" = "3" ]; then
+      echo -n true
+    else
+      echo -n "false"
+    fi
+  fi
+}
+
 function simulator_app_path {
   if [ "${DEVELOPER_DIR}" = "" ]; then
     local dev_dir=$(xcode-select --print-path)
