d86e57609768ada60fc7d8263862b70191d648d4
Joshua Moody <joshuajmoody@gmail.com>
2018-03-07 10:52:02 +0100

    Server: remove unused 'resolve' code from Application

--- Server/Application/Application.m
+++ Server/Application/Application.m
@@ -124,63 +124,6 @@ static Application *currentApplication;
     [currentApplication startSession];
 }
 
-+ (id)applicationQuery:(XCUIApplication *)xcuiApplication {
-    SEL selector = NSSelectorFromString(@"applicationQuery");
-
-    NSMethodSignature *signature;
-    Class klass = NSClassFromString(@"XCUIApplication");
-    signature = [klass instanceMethodSignatureForSelector:selector];
-    NSInvocation *invocation;
-
-    invocation = [NSInvocation invocationWithMethodSignature:signature];
-    invocation.target = xcuiApplication;
-    invocation.selector = selector;
-
-    id applicationQuery = nil;
-    void *buffer;
-    [invocation invoke];
-    [invocation getReturnValue:&buffer];
-    applicationQuery = (__bridge id)buffer;
-    return applicationQuery;
-}
-
-+ (void)resolveSnapshot:(XCUIApplication *)xcuiApplication {
-    if ([xcuiApplication lastSnapshot]) {
-        DDLogDebug(@"Resolving application snapshot: already has a 'last snapshot'; nothing to do");
-        return;
-    }
-
-    DDLogDebug(@"Resolving application snapshot: 'last snapshot' does not exist");
-    NSTimeInterval start = [[CBXMachClock sharedClock] absoluteTime];
-
-    id applicationQuery = [Application applicationQuery:xcuiApplication];
-
-    Class klass = NSClassFromString(@"XCApplicationQuery");
-    SEL selector = NSSelectorFromString(@"elementBoundByIndex:");
-
-    NSMethodSignature *signature;
-    signature = [klass instanceMethodSignatureForSelector:selector];
-    NSInvocation *invocation;
-
-    invocation = [NSInvocation invocationWithMethodSignature:signature];
-    invocation.target = applicationQuery;
-    invocation.selector = selector;
-    NSUInteger index = 0;
-    [invocation setArgument:&index atIndex:2];
-
-    XCUIElement *element = nil;
-    void *buffer;
-    [invocation invoke];
-    [invocation getReturnValue:&buffer];
-    element = (__bridge XCUIElement *)buffer;
-
-    [element resolve];
-
-    NSTimeInterval end = [[CBXMachClock sharedClock] absoluteTime];
-    NSTimeInterval elapsed = end - start;
-    DDLogDebug(@"Took %@ seconds to resolve application snapshot", @(elapsed));
-}
-
 + (NSDictionary *)tree {
     XCUIApplication *app = [Application currentApplication];
     [XCUIApplication cbxResolveSnapshot:app];
