398c4adbd2641216e5364614d28493dd385c5050
Tikhon Polyakov <polyakrecords@gmail.com>
2018-05-08 17:15:45 +0700

    Bin: change appcenter build script credentials logic

--- bin/appcenter.sh
+++ bin/appcenter.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 source bin/log.sh
-source bin/credentials.sh
 
 set -e
 
@@ -11,6 +10,7 @@ if [ -z ${1} ]; then
 Examples:
 
 $ bin/appcenter.sh e9232255
+$ bin/appcenter.sh organization/device-set
 $ SKIP_IPA_BUILD=1 SERIES='Args and env' bin/appcenter.sh e9232255
 $ SERIES='DeviceAgent 2.0' bin/appcenter.sh e9232255 48d137d6228ccda303b2a71b0d09e1d0629bf980
 
@@ -28,23 +28,32 @@ SKIP_IPA_BUILD: iff 1, then skip re-building the ipa.
                 'make test-ipa' will still be called, so changes in the
                 features/ directory will be staged and sent to Test Cloud.
 BUILD_RUN_LOOP: iff 1, then rebuild run-loop gem before uploading.
-BUILD_CALABASH: iff 1, then rebuild Calabash iOS gem before uploading.
-:
+
 "
 
   exit 64
 fi
+
+CREDS=.appcenter-credentials
+if [ ! -e "${CREDS}" ]; then
+  error "This script requires a ${CREDS} file"
+  error "Generating a template now:"
+  cat >${CREDS} <<EOF
+export APPCENTER_TOKEN=
+EOF
+  cat ${CREDS}
+  error "Update the file with your credentials and run again."
+  error "Bye."
+  exit 1
+fi
+
+source "${CREDS}"
 
 # The uninstall/install dance is required to test changes in
 # run-loop and calabash-cucumber in Test Cloud
 if [ "${BUILD_RUN_LOOP}" = "1" ]; then
   gem uninstall -Vax --force --no-abort-on-dependent run_loop
   (cd ../run_loop; rake install)
-fi
-
-if [ "${BUILD_CALABASH}" = "1" ]; then
-  gem uninstall -Vax --force --no-abort-on-dependent calabash-cucumber
-  (cd ../calabash-ios/calabash-cucumber; rake install)
 fi
 
 PREPARE_TC_ONLY="${SKIP_IPA_BUILD}" make test-ipa
@@ -76,13 +85,12 @@ appcenter test run calabash \
   --app-path testcloud-submit/TestApp.ipa \
   --app App-Center-Test-Cloud/DeviceAgent-TestApp \
   --project-dir testcloud-submit \
-  --token $AC_TOKEN \
+  --token $APPCENTER_TOKEN \
   --devices "${1}" \
   --config-path cucumber.yml \
   --profile default \
-  --dsym-dir testcloud-submit/TestApp.app.dSYM \
   --include .xtc \
   --test-parameter ${INJECT} \
   --test-parameter ${APP_ENV} \
-  --test-series ${SERIES}
+  --test-series ${SERIES} \
   --disable-telemetry
