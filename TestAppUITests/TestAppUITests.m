280c152dfb03dfda06506b68e2befca831420b25
Sergey Dolin <sergey@s4y.solutions>
2019-07-24 21:27:05 +0500

    rm boileplate comments

--- TestAppUITests/TestAppUITests.m
+++ TestAppUITests/TestAppUITests.m
@@ -2,9 +2,6 @@
 //  TestAppUITests.m
 //  TestAppUITests
 //
-//  Created by  Sergey Dolin on 23/07/2019.
-//  Copyright © 2019 Calabash. All rights reserved.
-//
 
 #import <XCTest/XCTest.h>
 
@@ -15,20 +12,13 @@
 @implementation TestAppUITests
 
 - (void)setUp {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-
-    // In UI tests it is usually best to stop immediately when a failure occurs.
     self.continueAfterFailure = YES;
-
-    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
 }
 
 - (void)tryUIWebViewOfRowID:(NSString*) rowId {
-    // UI tests must launch the application that they test.
     XCUIApplication *app = [[XCUIApplication alloc] init];
     [app launch];
 
