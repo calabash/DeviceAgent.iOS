185fc8a4924bded34e0f0a3c1acfbad77efeb934
Sergey Dolin <v-sedoli@microsoft.com>
2019-09-02 22:15:12 +0500

    Fix build

--- Server/Utilities/SpringBoardAlerts.m
+++ Server/Utilities/SpringBoardAlerts.m
@@ -18,6 +18,7 @@ static SpringBoardAlert *alert(NSString *buttonTitle, BOOL shouldAccept, NSStrin
 @property(strong) NSArray<SpringBoardAlert *> *alerts;
 
 - (instancetype)init_private;
+
 @end
 
 @implementation SpringBoardAlerts
@@ -31,25 +32,26 @@ static SpringBoardAlert *alert(NSString *buttonTitle, BOOL shouldAccept, NSStrin
 - (instancetype)init_private {
     self = [super init];
     if (self) {
-        _alerts = [self concatenateArrays:@[
-                                            [self USEnglishAlerts],
-                                            [self DanishAlerts],
-                                            [self DutchAlerts],
-                                            [self DutchAlerts_BE],
-                                            [self SwedishAlerts],
-                                            [self EUSpanishAlerts],
-                                            [self ES419SpanishAlerts],
-                                            [self GermanAlerts],
-                                            [self FrenchAlerts],
-                                            [self RussianAlerts],
-                                            [self PTBrazilAlerts],
-                                            [self KoreanAlerts],
-                                            [self ItalianAlerts],
-                                            [self HebrewAlerts],
-                                            [self GreekAlerts],
-                                            [self HungarianAlerts],
-                                            [self CzechAlerts]
-                                            ]];
+        NSDataAsset *asset = [[NSDataAsset alloc] initWithName:@"alerts"];
+        NSDictionary * languages = [NSJSONSerialization JSONObjectWithData:[asset data] options:kNilOptions error:nil];
+        
+        NSMutableArray<SpringBoardAlert *> * result = [NSMutableArray<SpringBoardAlert *> array];
+        NSEnumerator * languagesEnumerator = [languages keyEnumerator];
+        id language;
+        
+        while ((language = [languagesEnumerator nextObject])) {
+            NSArray * alerts = [languages objectForKey: language];
+            for (int i=0; i < alerts.count; i++) {
+                NSDictionary* alertDict = alerts[i];
+                NSString * title = [alertDict objectForKey:@"title"];
+                NSArray * buttons = [alertDict objectForKey:@"buttons"];
+                id shouldAccept = [alertDict objectForKey: @"shouldAccept"];
+                if (title != nil && shouldAccept != nil && buttons != nil && buttons.count > 0 ){
+                    [result addObject: alert(buttons[0], [shouldAccept boolValue], title)];
+                }
+            }
+        }
+        _alerts =  [NSArray<SpringBoardAlert *> arrayWithArray: result];
     }
     return self;
 }
@@ -77,35 +79,5 @@ static SpringBoardAlert *alert(NSString *buttonTitle, BOOL shouldAccept, NSStrin
     return match;
 }
 
-- (NSArray<SpringBoardAlert *> *)concatenateArrays:(NSArray<NSArray *> *)arrays {
-    __block NSArray *final = @[];
-    [arrays enumerateObjectsUsingBlock:^(NSArray *array, NSUInteger idx, BOOL *stop) {
-        final = [final arrayByAddingObjectsFromArray:array];
-    }];
-    return final;
-}
-
-- (NSArray<SpringBoardAlert *> *)USEnglishAlerts {
-    NSDataAsset *asset = [[NSDataAsset alloc] initWithName:@"alerts"];
-    NSDictionary * languages = [NSJSONSerialization JSONObjectWithData:[asset data] options:kNilOptions error:nil];
-
-    NSMutableArray<SpringBoardAlert *> * result = [NSMutableArray<SpringBoardAlert *> array];
-    NSEnumerator * languagesEnumerator = [languages keyEnumerator];
-    id language;
-    
-    while ((language = [languagesEnumerator nextObject])) {
-        NSArray * alerts = [languages objectForKey: language];
-        for (int i=0; i < alerts.count; i++) {
-            NSDictionary* alertDict = alerts[i];
-            NSString * title = [alertDict objectForKey:@"title"];
-            NSArray * buttons = [alertDict objectForKey:@"buttons"];
-            id shouldAccept = [alertDict objectForKey: @"shouldAccept"];
-            if (title != nil && shouldAccept != nil && buttons != nil && buttons.count > 0 ){
-                [result addObject: alert(buttons[0], shouldAccept, title)];
-            }
-        }
-    }
-    return [NSArray<SpringBoardAlert *> arrayWithArray: result];
-}
 
 @end
