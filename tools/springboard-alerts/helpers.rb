114a9e064aa4ead4581c431f9c70909fade03ed9
Maxim Lobanov <v-malob@microsoft.com>
2019-09-02 16:10:35 +0300

    small refactoring;update framework file

--- tools/springboard-alerts/helpers.rb
+++ tools/springboard-alerts/helpers.rb
@@ -1,7 +1,5 @@
 def read_json(file_path)
-    unless File.exist?(file_path)
-        raise "The file '#{file_path}' is not found"
-    end
+    raise "The file '#{file_path}' is not found" unless File.exist?(file_path)
 
     input_file = File.read(file_path)
     input_data = JSON.parse(input_file)
@@ -9,15 +7,12 @@ def read_json(file_path)
 end
 
 def read_strings(file_path)
-    unless File.exist?(file_path)
-        raise "The file '#{file_path}' is not found"
-    end
+    raise "The file '#{file_path}' is not found" unless File.exist?(file_path)
 
     args = ['plutil', '-convert', 'json', '-o', '"-"', "\"#{file_path}\""].join(' ')
     content = `#{args}`
     JSON.parse(content)
 end
-    
 
 def save_to_json_file(file_path, content)
     # create directory if doesn't exist
