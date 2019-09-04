b6089447a2226451476e902f1e2139f8ce077957
Maxim Lobanov <v-malob@microsoft.com>
2019-09-04 14:26:01 +0300

    fix indent

--- tools/springboard-alerts/localization_storage.rb
+++ tools/springboard-alerts/localization_storage.rb
@@ -3,63 +3,63 @@ require_relative 'helpers'
 # Storage class that helps to extend existing localization database with new values
 # Use json format to store database
 class LocalizationStorage
-    def initialize(storage_path, languages)
-        @storage_path = File.expand_path(storage_path)
-        @languages = languages.map { |lang| lang['name'] }
-        load
+  def initialize(storage_path, languages)
+    @storage_path = File.expand_path(storage_path)
+    @languages = languages.map { |lang| lang['name'] }
+    load
+  end
+
+  # Adds new alert with specific language and title to the database
+  def add_entry(language, title, button)
+    @storage[language] ||= []
+
+    alert_entry = add_alert(language, title)
+    add_button_to_alert(alert_entry, button) if button
+  end
+
+  # Saves database to the local disk
+  def save
+    @languages.each do |lang|
+      language_path = get_local_language_path(lang)
+      save_json(language_path, @storage[lang])
     end
+  end
 
-    # Adds new alert with specific language and title to the database
-    def add_entry(language, title, button)
-        @storage[language] ||= []
+  private
 
-        alert_entry = add_alert(language, title)
-        add_button_to_alert(alert_entry, button) if button
+  # Adds new alert if it doesn't exist yet
+  def add_alert(language, title)
+    alert_entry = @storage[language].find { |item| item['title'] == title }
+    return alert_entry if alert_entry
+
+    alert_entry = {
+      'title' => title,
+      'buttons' => [],
+      'shouldAccept' => true
+    }
+    @storage[language].push(alert_entry)
+    alert_entry
+  end
+
+  # Adds button to the existing alert if it doesn't contain it
+  def add_button_to_alert(alert_entry, button)
+    button_entry = alert_entry['buttons'].find { |v| v == button }
+    alert_entry['buttons'].push(button) if button_entry.nil?
+  end
+
+  # Returns local disk path for specific language
+  def get_local_language_path(language)
+    # DeviceAgent.iOS/Server/Resources.xcassets/springboard-alerts/springboard-alerts-en.dataset/alerts.json
+    File.join(@storage_path, "springboard-alerts-#{language}.dataset/alerts.json")
+  end
+
+  # Loads database from the local disk
+  def load
+    @storage = {}
+    @languages.each do |lang|
+      language_path = get_local_language_path(lang)
+      @storage[lang] ||= []
+      @storage[lang] = read_json(language_path) if File.exist?(language_path)
     end
-
-    # Saves database to the local disk
-    def save
-        @languages.each do |lang|
-            language_path = get_local_language_path(lang)
-            save_json(language_path, @storage[lang])
-        end
-    end
-
-    private
-
-    # Adds new alert if it doesn't exist yet
-    def add_alert(language, title)
-        alert_entry = @storage[language].find { |item| item['title'] == title }
-        return alert_entry if alert_entry
-
-        alert_entry = {
-            'title' => title,
-            'buttons' => [],
-            'shouldAccept' => true
-        }
-        @storage[language].push(alert_entry)
-        alert_entry
-    end
-
-    # Adds button to the existing alert if it doesn't contain it
-    def add_button_to_alert(alert_entry, button)
-        button_entry = alert_entry['buttons'].find { |v| v == button }
-        alert_entry['buttons'].push(button) if button_entry.nil?
-    end
-
-    # Returns local disk path for specific language
-    def get_local_language_path(language)
-        # DeviceAgent.iOS/Server/Resources.xcassets/springboard-alerts/springboard-alerts-en.dataset/alerts.json
-        File.join(@storage_path, "springboard-alerts-#{language}.dataset/alerts.json")
-    end
-
-    # Loads database from the local disk
-    def load
-        @storage = {}
-        @languages.each do |lang|
-            language_path = get_local_language_path(lang)
-            @storage[lang] ||= []
-            @storage[lang] = read_json(language_path) if File.exist?(language_path)
-        end
-    end
+  end
 end
--- tools/springboard-alerts/update-alerts.rb
+++ tools/springboard-alerts/update-alerts.rb
@@ -10,54 +10,54 @@ xcode = RunLoop::Xcode.new
 
 # Try to find specific framework under CoreSimulators directory
 def find_framework(root_path, framework_name)
-    found = Dir.glob("#{root_path}/**/#{framework_name}")
-    return nil if found.empty?
+  found = Dir.glob("#{root_path}/**/#{framework_name}")
+  return nil if found.empty?
 
-    raise "More than 1 occurrence of '#{framework_name}' is found; unexpected behavior" if found.length > 1
+  raise "More than 1 occurrence of '#{framework_name}' is found; unexpected behavior" if found.length > 1
 
-    found[0]
+  found[0]
 end
 
 # Gets valid language path
 # Some languages have a few aliases like 'en.lproj' and 'English.lproj', we should check all options
 def find_language(framework_path, language)
-    language['filenames'].find do |lang_alias|
-        language_path = File.join(framework_path, lang_alias)
-        return language_path if Dir.exist?(language_path)
-    end
+  language['filenames'].find do |lang_alias|
+    language_path = File.join(framework_path, lang_alias)
+    return language_path if Dir.exist?(language_path)
+  end
 
-    nil
+  nil
 end
 
 # Reads and parse all *.strings files in specific directory
 def collect_localization_dictionary(language_dir_path)
-    dict = {}
-    Dir.glob("#{language_dir_path}/*.strings") do |file_path|
-        pairs = read_strings(file_path)
-        dict.merge!(pairs)
-    end
+  dict = {}
+  Dir.glob("#{language_dir_path}/*.strings") do |file_path|
+    pairs = read_strings(file_path)
+    dict.merge!(pairs)
+  end
 
-    dict
+  dict
 end
 
 # Iterate through required values for framework and try to get values from dict
 # Add found values to localization storage
 def pick_required_values(found_values_dict, target_framework, localization_storage, language)
-    framework_name = target_framework['name']
-    target_framework['values'].each do |item|
-        title = item['title']
-        button = item['button']
-        title_value = found_values_dict[title]
-        button_value = found_values_dict[button]
+  framework_name = target_framework['name']
+  target_framework['values'].each do |item|
+    title = item['title']
+    button = item['button']
+    title_value = found_values_dict[title]
+    button_value = found_values_dict[button]
 
-        puts "Unknown button constant '#{button}' for framework '#{framework_name}'".yellow if button && !button_value
+    puts "Unknown button constant '#{button}' for framework '#{framework_name}'".yellow if button && !button_value
 
-        if title_value
-            localization_storage.add_entry(language, title_value, button_value)
-        else
-            puts "Unknown alert constant '#{title}' for framework '#{framework_name}'".red
-        end
+    if title_value
+      localization_storage.add_entry(language, title_value, button_value)
+    else
+      puts "Unknown alert constant '#{title}' for framework '#{framework_name}'".red
     end
+  end
 end
 
 target_frameworks = read_json('frameworks.json')
@@ -65,34 +65,34 @@ languages = read_json('languages.json')
 localization_storage = LocalizationStorage.new('../../Server/Resources.xcassets/springboard-alerts', languages)
 
 target_frameworks.each do |framework|
-    puts "Scan framework '#{framework['name']}'".green
-    framework_path = find_framework(xcode.core_simulator_dir, framework['name'])
+  puts "Scan framework '#{framework['name']}'".green
+  framework_path = find_framework(xcode.core_simulator_dir, framework['name'])
 
-    unless framework_path && Dir.exist?(framework_path)
-        puts "Skip '#{framework['name']}' since it was not found".red
-        next
+  unless framework_path && Dir.exist?(framework_path)
+    puts "Skip '#{framework['name']}' since it was not found".red
+    next
+  end
+
+  puts "Found on path '#{framework_path}'"
+
+  available_languages = languages.select { |language| find_language(framework_path, language) }
+  skipped_languages = languages - available_languages
+  # puts "Available languages: #{available_languages.map { |lang| lang['name'] }}"
+  puts "Skipped languages: #{skipped_languages.map { |lang| lang['name'] }}".yellow unless skipped_languages.empty?
+
+  available_languages.each do |language|
+    language_name = language['name']
+    language_path = find_language(framework_path, language)
+
+    found_values_dict = collect_localization_dictionary(language_path)
+    pick_required_values(found_values_dict, framework, localization_storage, language_name)
+
+    # debug info
+    if ENV['DEBUG'] && language_name == 'en'
+      report_path = "reports/#{framework['name']}.#{language_name}.json"
+      save_json(report_path, found_values_dict)
     end
-
-    puts "Found on path '#{framework_path}'"
-
-    available_languages = languages.select { |language| find_language(framework_path, language) }
-    skipped_languages = languages - available_languages
-    # puts "Available languages: #{available_languages.map { |lang| lang['name'] }}"
-    puts "Skipped languages: #{skipped_languages.map { |lang| lang['name'] }}".yellow unless skipped_languages.empty?
-
-    available_languages.each do |language|
-        language_name = language['name']
-        language_path = find_language(framework_path, language)
-
-        found_values_dict = collect_localization_dictionary(language_path)
-        pick_required_values(found_values_dict, framework, localization_storage, language_name)
-
-        # debug info
-        if ENV['DEBUG'] && language_name == 'en'
-            report_path = "reports/#{framework['name']}.#{language_name}.json"
-            save_json(report_path, found_values_dict)
-        end
-    end
+  end
 end
 
 # save updated database to the local file
