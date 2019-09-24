1bb0a582312b180c0adf514e89a66d264e60ca45
Vladimir Safonkin <vsafonkin@gmail.com>
2019-09-24 16:50:46 +0300

    small fix

--- cucumber/features/steps/query.rb
+++ cucumber/features/steps/query.rb
@@ -312,7 +312,8 @@ Then(/^I get Text Field by \"(.+)\" query with string \"(.+)\" and check value \
     actual = wait_for_view({id: string_query})
   when "text"
     actual = wait_for_view({text: string_query})
-
+  end
+  
   expect(actual["value"]).to be == value
 end
 
