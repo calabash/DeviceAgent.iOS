da8a35798c4320ad9720a33a1a83cece5d8afc97
Ilya Bausov <ilya.bausov@akvelon.com>
2021-02-18 07:01:54 +0300

    Increased timeout for simulator start process to 10 minutes.

--- cucumber/features/steps/shared.rb
+++ cucumber/features/steps/shared.rb
@@ -7,7 +7,7 @@ module DeviceAgent
       return true if DeviceAgent::Shared.class_variable_get(:@@app_ready)
 
       if device_info["simulator"]
-        timeout = 120
+        timeout = 600
       else
         timeout = 20
       end
