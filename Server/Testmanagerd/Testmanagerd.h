12f224e0df0a8fd52e2d04d38b850bda99b66ebf
AlexeyBudynkov <77280103+AlexeyBudynkov@users.noreply.github.com>
2021-05-17 14:56:06 +0300

    Notes in comments are changed

--- Server/Testmanagerd/Testmanagerd.h
+++ Server/Testmanagerd/Testmanagerd.h
@@ -10,10 +10,20 @@
 // It runs on the device and has priviledges for interacting with the entire device,
 // and thus is the core component of XCUITest's underlying gesture synthesis.
 //
-// It exposes an API via the `XCTestManager_ManagerInterface` which is used to
+// Early it exposed an API via the `XCTestManager_ManagerInterface` which is used to
 // interact with it and by extension the device. Once and NSXPCConnection is
 // established with the dæmon, automation commands can be freely sent.
 // */
+// After Xcode 12.5 XCTestManager_ManagerInterface is splited into several interfaces:
+//XCTMessagingRole_EventSynthesis
+//XCTMessagingRole_SiriAutomation
+//XCTMessagingRole_BundleRequesting
+//XCTMessagingRole_MemoryTesting
+//XCTMessagingRole_ForcePressureSupportQuerying
+//XCTMessagingRole_CapabilityExchange
+//
+//Right now only two of them are used: XCTMessagingRole_EventSynthesis, XCTMessagingRole_CapabilityExchange
+//That's how it looked before:
 //@interface Testmanagerd : NSObject
 //
 ///**
@@ -30,7 +40,13 @@
 #import "XCTMessagingRole_EventSynthesis-Protocol.h"
 
 @interface Testmanagerd_EventSynthesis : NSObject
-
+//
+///**
+// Return a singleton instance of the Testmanagerd proxy object, on which
+// raw testmanagerd methods may be invoked. Note that these are not all entirely
+// documented, as it is a private API and we have yet to fully understand all
+// of the methods.
+// */
 + (id<XCTMessagingRole_EventSynthesis>)get;
 
 @end
@@ -38,7 +54,13 @@
 #import "XCTMessagingRole_CapabilityExchange-Protocol.h"
 
 @interface Testmanagerd_CapabilityExchange : NSObject
-
+//
+///**
+// Return a singleton instance of the Testmanagerd proxy object, on which
+// raw testmanagerd methods may be invoked. Note that these are not all entirely
+// documented, as it is a private API and we have yet to fully understand all
+// of the methods.
+// */
 + (id<XCTMessagingRole_CapabilityExchange>)get;
 
 @end
