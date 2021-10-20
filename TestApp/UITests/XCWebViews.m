f29483001c308051d3424ef007dacb09a845c5cc
AlexeyBudynkov <77280103+AlexeyBudynkov@users.noreply.github.com>
2021-10-20 13:31:09 +0300

    Fixed a test with webview - there was read until writing is finished

--- TestApp/UITests/XCWebViews.m
+++ TestApp/UITests/XCWebViews.m
@@ -14,6 +14,21 @@
 - (void)tearDown {
 }
 
+- (BOOL)spinRunLoopWithTimeout:(NSTimeInterval)timeout untilTrue:( BOOL (^)(void) )untilTrue
+{
+  NSDate *date = [NSDate dateWithTimeIntervalSinceNow:timeout];
+  while (!untilTrue()) {
+    @autoreleasepool {
+      if (timeout > 0 && [date timeIntervalSinceNow] < 0) {
+        return NO;
+      }
+      // Wait for 100ms
+        [NSRunLoop.currentRunLoop runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.1]];
+    }
+  }
+  return YES;
+}
+
 - (void)tryUIWebViewOfRowID:(NSString*) rowId {
     XCUIApplication *app = [[XCUIApplication alloc] init];
     [app launch];
@@ -32,6 +47,10 @@
     [firstname tap];
     [firstname typeText:@"123"];
 
+    [self spinRunLoopWithTimeout:2 untilTrue:^BOOL{
+        return [firstname.value  isEqual: @"123"];
+    }];
+    
     NSString* v0 = firstname.value;    // v0    __NSCFString *    @"\U0000fffc"    0x0000600002d19a00
     XCTAssertEqualObjects(v0, @"123"); // ((v0) equal to (@"123")) failed: ("￼") is not equal to ("123")
 }
