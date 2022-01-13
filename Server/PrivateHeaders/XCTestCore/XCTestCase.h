c89ed32e378f814b9e3c5fe544e07e610c2d7c5a
Ilya Bausov <ilya.bausov@akvelon.com>
2022-01-13 11:53:34 +0300

    PrivateHeaders re-update Part 2.

--- Server/PrivateHeaders/XCTestCore/XCTestCase.h
+++ Server/PrivateHeaders/XCTestCore/XCTestCase.h
@@ -66,7 +66,7 @@
     NSDictionary *_testRunConfiguration;
 }
 
-@property(copy, getter=_activityAggregateStatistics, setter=_setActivityAggregateStatistics:) NSDictionary *activityAggregateStatistics;
+@property(readonly, getter=_activityAggregateStatistics) NSDictionary *activityAggregateStatistics;
 @property BOOL _didMeasureMetrics;
 @property BOOL _didStartMeasuring;
 @property BOOL _didStopMeasuring;
