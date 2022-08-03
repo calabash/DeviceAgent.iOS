6fd28d4961cd245ef37281bead7447b04cc3a523
Alexey Budynkov <>
2022-08-03 15:12:32 +0400

    remove an unrequired check return a en_US check

--- Server/Utilities/SpringBoardAlerts.m
+++ Server/Utilities/SpringBoardAlerts.m
@@ -108,10 +108,6 @@ static SpringBoardAlert *alert(NSArray *buttonTitles, BOOL shouldAccept, NSStrin
             [self loadLanguageIfExists:fixedPreferredLanguage:resultArray];
         }
         
-        // load "en" lang
-        if (![preferredLanguage hasPrefix:@"en"]) {
-            [self loadLanguageIfExists:@"en": resultArray];
-        }
         _alerts =  [NSArray<SpringBoardAlert *> arrayWithArray: resultArray];
         NSTimeInterval elapsedSeconds =
         [[CBXMachClock sharedClock] absoluteTime] - startTime;
@@ -124,7 +120,9 @@ static SpringBoardAlert *alert(NSArray *buttonTitles, BOOL shouldAccept, NSStrin
 // Some language names can be different in different Xcode versions
 // function maps language names to make sure that all xcode versions will work
 - (NSString *)fixLanguageName:(NSString *)languageName {
-    if ([languageName isEqualToString: @"zh-Hans-US"]) {
+    if ([languageName isEqualToString: @"en-US"]) {
+        return @"en";
+    } else if ([languageName isEqualToString: @"zh-Hans-US"]) {
         return @"zh-CN";
     } else if ([languageName isEqualToString: @"zh-Hant-US"]) {
         return @"zh-TW";
