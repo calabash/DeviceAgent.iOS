998fccb6f6072b8dcc90e4dda0e9d28f641f7792
Alexey Budynkov <>
2022-10-07 17:34:30 +0400

    Add some comments Remove unused script linking Comments cleaned

--- Server/FBWebDriverAgent/FBXCElementSnapshot.h
+++ Server/FBWebDriverAgent/FBXCElementSnapshot.h
@@ -15,15 +15,7 @@
 #import "CBX-XCTest-Umbrella.h"
 
 #if LOAD_XCTEST_PRIVATE_HEADERS == 0
-//
-//#import "XCUIElementAttributes-Protocol.h"
-//#import "XCUIApplication.h"
 
-//@protocol XCUIElementAttributes
-//@end
-
-//
-//#import <XCUIAutomation/XCUIElementAttributes-Protocol.h>
 #import <XCTest/XCTest.h>
 #import "CDStructures.h"
 
